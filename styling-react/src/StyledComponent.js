8e96b26d63e453ee7446f8251535fa521f308aa8
ChaMyungjun <acg70159@gmail.com>
2019-10-13 16:50:08 +0900

    upload

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 
+const size = {
+    desktop: 1024,
+    tablet: 768
+};
+
+//size 객체에 따라 자동으로 media 쿼리 함수 생성
+const media = Object.keys(size).reduce((acc, label) => {
+    acc[label] = (...args) => css `
+        @media (max-width: ${size[label] / 16}em) {
+            ${css(...args)}
+        }
+    `;
+
+    return acc;
+}, {});
+
 const Box = styled.div `
     /*props로 넣어 준 값을 직접 전달해 줄 수 있습니다. */
     background: ${props => props.color || 'blue'};
     padding: 1rem;
     display: flex;
+
+    /*기본적으로 가로크기 1024px 가운데정렬 가로 크기가 작아짐에 따라 크기를 줄이인다. 768px 미만이면 꽉 채운다. */
+
+    width: 1024px;
+    margin: 0 auto;
+    ${media.desktop`width: 768px;`}
+    ${media.tablet`width: 100%;`}
 `;
 
 const Button = styled.button `

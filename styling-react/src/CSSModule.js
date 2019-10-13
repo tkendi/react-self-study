5bf0d33b435620626f91a364275fae4a9290bfef
ChaMyungjun <acg70159@gmail.com>
2019-10-13 16:10:41 +0900

    upload

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
 
 const CSSModule = () => {
     return(
-        <div className = {styles.wrapper}>
+        <div className = {`${styles.wrapper} ${styles.inverted}`}>
             안녕하세요, 저는 <span className = "something">CSS Module</span>
         </div>
     );

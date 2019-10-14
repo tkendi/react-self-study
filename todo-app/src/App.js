28a0d792fdec5417aee843c898202afe15f6fd9c
ChaMyungjun <acg70159@gmail.com>
2019-10-14 12:29:25 +0900

    upload

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,8 +1,8 @@
-import React from 'raect';
+import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자</TodoTemplate>;
+  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
 };
 
 export default App;

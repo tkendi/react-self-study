380dfab639ca3e5063e51f814cdddb120ce2b3f1
ChaMyungjun <acg70159@gmail.com>
2019-10-14 16:00:25 +0900

    upload

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보자',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보자',
+      checked: false,
+    },
+  ]);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList todos = {todos} />
+    </TodoTemplate>
+  );
 };
 
 export default App;

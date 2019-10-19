4fa2df701713d331ed3802e10f53f86c6559a798
ChaMyungjun <acg70159@gmail.com>
2019-10-19 20:09:49 +0900

    middleware  elementary

---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
+
+serviceWorker.unregister();

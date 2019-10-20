fe34193fe69649c71df146cb67431e3e1eeaba0c
ChaMyungjun <acg70159@gmail.com>
2019-10-20 17:22:10 +0900

    Middleware 비동기화 작업

--- learn-redux-middleware/src/containers/SampleContainer.js
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Sample from '../components/Sample';
-import {getPost, getUsers} from '../components/Sample';
+import {getPost, getUsers} from '../modules/sample';
 
 const {useEffect} = React;
 const SampleContainer = ({

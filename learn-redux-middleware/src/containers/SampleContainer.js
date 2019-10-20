15834e3cd9510c634538889a8cf4bdc407354d4e
ChaMyungjun <acg70159@gmail.com>
2019-10-20 17:13:35 +0900

    Middleware 비동기 작업처리

---
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import Sample from '../components/Sample';
+import {getPost, getUsers} from '../components/Sample';
+
+const {useEffect} = React;
+const SampleContainer = ({
+    getPost,
+    getUsers,
+    post,
+    users,
+    loadingPost,
+    loadingUsers
+}) => {
+    //클래스 형태의 컴포넌트 였으면 componentDidMount
+    useEffect(() => {
+        getPost(1);
+        getUsers(1);
+    }, [getPost, getUsers]);
+    return (
+        <Sample
+            post = {post}
+            users = {users}
+            loadingPost = {loadingPost}
+            loadingUsers = {loadingUsers}
+        />
+    );
+};
+
+export default connect (
+    ({sample}) => ({
+        post: sample.post,
+        users: sample.users,
+        loadingPost: sample.loading.GET_POST,
+        loadingUsers: sample.loading.GET_USERS
+    }),
+    {
+        getPost,
+        getUsers
+    }
+)(SampleContainer);

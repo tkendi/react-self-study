c83fb3370c7599d1cc02b029f1f6463f3112b50d
ChaMyungjun <acg70159@gmail.com>
2019-10-21 18:18:33 +0900

    Middleware를 통한 비동기 작업

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import CounterContainer from './containers/CounterContainer';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <CounterContainer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;

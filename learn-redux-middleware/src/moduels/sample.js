a1ea79a8925df85d07aaa2cc579d151e49396ac6
ChaMyungjun <acg70159@gmail.com>
2019-10-23 09:11:02 +0900

    Redux-Saga 리팩토링

--- learn-redux-middleware/src/moduels/sample.js
+++ learn-redux-middleware/src/moduels/sample.js
@@ -7,11 +7,9 @@ import createRequesetThunk from '../lib/createRequestThunk';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 //thunk 함수를 생성
 //thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치한다

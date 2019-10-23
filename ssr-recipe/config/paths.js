24c1a7cbc195433310b7477c4bb1d3badc607c53
ChaMyungjun <acg70159@gmail.com>
2019-10-23 23:30:43 +0900

    웹팩 환경설정

--- ssr-recipe/config/paths.js
+++ ssr-recipe/config/paths.js
@@ -83,6 +83,8 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
   servedPath: getServedPath(resolveApp('package.json')),
+  ssrIndexJS: resolveApp('src/index.server.js'),  //서버 사이드 렌더링 엔트리
+  ssrBuild: resolveApp('dist'),    //웹팩 처리 후 경로 저장
 };
 
 

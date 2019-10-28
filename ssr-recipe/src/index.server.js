351a1068644072339ff3d39940b7e0949cc350ad
ChaMyungjun <acg70159@gmail.com>
2019-10-28 15:04:16 +0900

    서버 사이드 렌더링 안됨

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import express from 'express';
-import {StaticRouter} from 'react-router-dom';
+import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
+import fs from 'fs';
+
+//asset-mainfest.json에서 파일 경로조회
+const manifest = JSON.parse(
+    fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
+);
+
+const chunks = Object.keys(manifest.files)
+    .filter(key => /chunk\.js$/.exec(key))  //chunk.js로 끝나는 키를 찾아서
+    .map(key => `<script src = "${manifest[key]}></script>`)    //스크립트 태그로 변환
+    .join('');  //합침
+
+function createPage(root) {
+    return `<!DOCType html>
+    <html lang = "en">
+    <head>
+        <meta charset = "utf-8" />
+        <link rel = "shortcut icon" href = "/favicon.ico" />
+        <meta
+            name = "viewport"
+            content = "width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name = "theme-color" content = "#000000" />
+        <title>React App </title>
+        <link href = "${manifest['main.css']}" rel = "stylesheet" />
+    </head>
+    <body>
+        <noscript> you need to enable JavaScript to run this app. </noscript>
+        <div id = "root">
+            ${root}
+        </div>
+        <script src = "${manifest['runtime-main.js']}"></script>
+            ${chunks}
+        <script src = "${manifest['main.js']}"></script>
+    </body>
+    </html>
+        `;
+}
 
 const app = express();
 
@@ -12,12 +50,12 @@ const serverRender = (req, res, next) => {
     //이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 한다
     const context = {};
     const jsx = (
-        <StaticRouter location = {req.url} context = {context}>
+        <StaticRouter location={req.url} context={context}>
             <App />
         </StaticRouter>
     );
     const root = ReactDOMServer.renderToString(jsx);    //렌더링을 하고
-    res.send(root);         //클라이언트에게 결과물을 응답한다
+    res.send(createPage(root));         //결과물 응답       
 };
 
 const serve = express.static(path.resolve('./build'), {

4e2e5c29c84e105c1d032aed588978e8ff4fc7e0
ChaMyungjun <acg70159@gmail.com>
2020-02-10 19:15:06 +0900

    react-self-study

--- blog/blog-backend/src/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-global-assign */
-//이 파일에서만 no-global-assign ESLint 옵션을 비활성화 합니다.
-// eslint-disable-next-line no-global-assign
-
-require = require('esm')(module /*, options */);
-module.exports = require('./main');

85a24417b8255e633d05469d4c121495f98d4d88
ChaMyungjun <acg70159@gmail.com>
2020-02-10 18:55:59 +0900

    react-self-study

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -10,34 +10,15 @@ const App = () => {
     <div>
       <ul>
         <li>
-          <Link to="/">홈</Link>
+          <Link to = "/">홈</Link>
         </li>
         <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/profiles">프로필</Link>
-        </li>
-        <li>
-          <Link to="/history">History 예제</Link>
+          <Link to = "/about">소개</Link>
         </li>
       </ul>
       <hr />
-      <Switch>
-        <Route path="/" component={Home} exact={true} />
-        <Route path={["/about", "/info"]} component={About} />
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={HistorySample} />
-        <Route
-          //path를 따로 정의하지 않으면 모든 상황에서 렌더링됨
-          render={({ location }) => (
-            <div>
-              <h2>이 페이지는 존재 하지 않습니다:</h2>
-              <p>{location.pathname}</p>
-            </div>
-          )}
-        />
-      </Switch>
+      <Route path = "/" component = {Home} exact = {true} />
+      <Route path = "/about" component = {About} />
     </div>
   );
 };

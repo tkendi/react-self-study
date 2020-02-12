1675c49f637bb809f45dddbbef7a9535bc94e286
ChaMyungjun <acg70159@gmail.com>
2020-02-12 14:59:26 +0900

    reqct-self-study

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
-import NewsPage from './pages/NewsPage';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewPage';
 
 const App = () => {
   return <Route path = "/:category?" component = {NewsPage} />;

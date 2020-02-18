8b5e929b8114213c75b19e8557f22366cd446fd8
ChaMyungjun <acg70159@gmail.com>
2020-02-18 16:10:29 +0900

    react-self-study

--- management/src/App.js
+++ management/src/App.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
+import Paper from '@material-ui/core/Paper'
 import Customer from "./components/Customer";
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
 
 const customers = [
   {
@@ -29,9 +47,23 @@ const customers = [
   }
 ];
 
-function App() {
+class App extends Component {
+  render() {
+    const { classes } = this.props;
   return (
-    <div>
+    <Paper className = {classes.root}>
+      <Table className = {classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
       {customers.map(c => {
         return <Customer 
           key = {c.id}
@@ -42,9 +74,12 @@ function App() {
           gender = {c.gender}
           job = {c.job}
         />;
-      })}
-    </div>
-  );
+      })}  
+      </TableBody>  
+      </Table>
+    </Paper>
+    );
+    }
 }
 
-export default App;
+export default withStyles(styles)(App);

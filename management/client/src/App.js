81b13ff05f27e618a503fd8807aded617bc19234
ChaMyungjun <acg70159@gmail.com>
2020-02-19 01:11:13 +0900

    react-self-study

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -19,34 +19,24 @@ const styles = theme => ({
   }
 });
 
-const customers = [
-  {
-    id: 1,
-    imgae: "https://placeimg.com/64/64/1",
-    name: "홍길동",
-    birthday: "961222",
-    gender: "남자",
-    job: "대학생"
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "홍길동",
-    birthday: "693322",
-    gender: "남자",
-    job: "대학생"
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "Lee",
-    birthday: "152362",
-    gender: "남자",
-    job: "qudtls"
-  }
-];
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then (res => this.setState({customers: res}))
+    .catch(err => console.log(err))
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -63,7 +53,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {
+            {this.state.customers ? this.state.customers.map(c => {
               return (
                 <Customer
                   key={c.id}
@@ -75,7 +65,7 @@ class App extends Component {
                   job={c.job}
                 />
               );
-            })}
+            }): ""}
           </TableBody>
         </Table>
       </Paper>

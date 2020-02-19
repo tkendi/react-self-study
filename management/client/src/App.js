8102ecd2bde27befee9b9ed54ff78088c0916b32
ChaMyungjun <acg70159@gmail.com>
2020-02-20 00:53:48 +0900

    react-self-study

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -5,9 +5,10 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 import Customer from "./components/Customer";
+import CustomerAdd from "./components/CustomerAdd";
 
 const styles = theme => ({
   root: {
@@ -25,69 +26,91 @@ const styles = theme => ({
 
 class App extends Component {
 
-  state = {
-    customers: "",
-    completed: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .then(err => console.log(err));
   }
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
-    .then (res => this.setState({customers: res}))
-    .catch(err => console.log(err));
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/customers');
+    const response = await fetch("/api/customers");
     const body = await response.json();
     return body;
-  }
+  };
 
   progress = () => {
-    const {completed} = this.state;
-    this.setState({completed: completed >= 100 ? 0 : completed + 1 });
-  }
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
 
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map(c => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            }): 
-            <TableRow>
-              <TableCell colSpan = "6" align = "center">
-                <CircularProgress className = {classes.progress} variant = "determinate"  value = {this.state.completed} />
-              </TableCell>
-            </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? (
+                this.state.customers.map(c => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress
+                      className={classes.progress}
+                      variant="determinate"
+                      value={this.state.completed}
+                    />
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh = {this.stateRefresh} />
+      </div>
     );
   }
 }
 
-export default withStyles(styles) (App);
+export default withStyles(styles)(App);
--- management/server.js
+++ management/server.js
@@ -1,38 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = require("fs");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
 
-app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          id: 1,
-          imgae: "https://placeimg.com/64/64/1",
-          name: "홍길동",
-          birthday: "961222",
-          gender: "남자",
-          job: "대학생"
-        },
-        {
-          id: 2,
-          image: "https://placeimg.com/64/64/2",
-          name: "홍길동",
-          birthday: "693322",
-          gender: "남자",
-          job: "대학생"
-        },
-        {
-          id: 3,
-          image: "https://placeimg.com/64/64/3",
-          name: "Lee",
-          birthday: "152362",
-          gender: "남자",
-          job: "qudtls"
-        }
-      ]);
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
+const multer = require('multer');
+const upload = multer({dest: './upload'});
+
+app.get("/api/customers", (req, res) => {
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+      console.log(err);
+      console.log(rows);
+    })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

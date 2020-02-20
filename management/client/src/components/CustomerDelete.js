c8bd0001581ea9ff2c8976dc7de0eeed5eb5d9b0
ChaMyungjun <acg70159@gmail.com>
2020-02-20 16:01:23 +0900

    react-self-study

---
+++ management/client/src/components/CustomerDelete.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { DialogContentText } from '@material-ui/core';
+
+class CustomerDelete extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        })
+    }
+
+    handleClose = () => {
+        this.setState({
+            this.setState({
+                open: false
+            })
+        })
+    }
+
+    deleteCustomer(id) {
+        const url = '/api/customers/' + id;
+        fetch(url, {
+            method: 'DELETE'
+        });
+        this.props.stateRefresh();
+    }
+    render() {
+        return(
+            <div>
+                <Button variant = "contained" color = "secondary" onClick = {this.handleClickOpen}>삭제</button>
+                <Dialog open = {this.state.open} onClose = {this.handleClose} >
+                    <DialogTitle onClose = {tihs.handleClose}>
+                        삭제 경고
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography>
+                            선택한 고객 정보가 삭제 됩니다
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant = "contained" color = "primary" onClick = {(e) => {this.deleteCustomer(this.props.id)}}>삭제</Button>
+                        <Button variant = "outlined" color = "primary" onClick = {this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        )
+    }
+}
+
+export default CustomerDelete;
--- management/server.js
+++ management/server.js
@@ -21,7 +21,7 @@ const multer = require('multer');
 const upload = multer({dest: './upload'});
 
 app.get("/api/customers", (req, res) => {
-  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+  connection.query("SELECT * FROM CUSTOMER WHERE isDeleted = 0", (err, rows, fields) => {
     res.send(rows);
   });
 });
@@ -29,7 +29,7 @@ app.get("/api/customers", (req, res) => {
 app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
-  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
   let image = '/image/' + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
@@ -41,6 +41,16 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
       res.send(rows);
       console.log(err);
       console.log(rows);
+    })
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+  let params = [req.params.id]
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+      console.log(err);
     })
 })
 

af90dc07c518e56f1928a1dfa8bc43bcc501e495
ChaMyungjun <acg70159@gmail.com>
2020-03-08 22:12:35 +0900

    server test

--- corona/corona-server/routes/users.js
+++ corona/corona-server/routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+    {id: 1, username: "cha"},
+    {id: 2, username: "myung"}
+  ]);
 });
 
 module.exports = router;

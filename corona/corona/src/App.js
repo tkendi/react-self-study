a1026c868a3e6adc859d740ff31ccdb58f63ba43
ChaMyungjun <acg70159@gmail.com>
2020-03-14 02:02:32 +0900

    modify sidebar

--- corona/corona/src/App.js
+++ corona/corona/src/App.js
@@ -6,16 +6,17 @@ import Domestic from './components/Domestic';
 import { NavLink, Route } from 'react-router-dom';
 import Index_AppBar from './public/Index_AppBar';
 import Home from './components/Home';
-
+import Index_SideBar from './public/Index_SideBar';
 class App extends Component {
   render() {
     return (
       <>
         <div className="App">
           <Index_AppBar />
+          <Index_SideBar />
           <current_situation_char />
-          <Route path = "/" component = {Home }exact={true} />
-          <Route path="/City" component={City}/>
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/City" component={City} />
           <Route path="/Domestic" component={Domestic} />
         </div>
       </>
--- corona/corona/src/public/Index_AppBar.js
+++ corona/corona/src/public/Index_AppBar.js
@@ -3,20 +3,22 @@ import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import { Typography } from '@material-ui/core';
 import Clock from 'react-live-clock';
-import './css/Index_AppBar.css'
-import Index_SideBar from './Index_SideBar';
+import './css/Index_AppBar.css';
 
 class Index_AppBar extends Component {
   render() {
-      const { classes } = this.props
     return (
-      <div className = "root">
+      <div className="root">
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h4" className="title">
-            <Index_SideBar className="Index_AppBar" />
               Corona
-              <Clock format={'HH:mm'} ticking = {true} timezone = {'Asia/Seoul'} className = "time_zone" />
+              <Clock
+                format={'HH:mm'}
+                ticking={true}
+                timezone={'Asia/Seoul'}
+                className="time_zone"
+              />
             </Typography>
           </Toolbar>
         </AppBar>

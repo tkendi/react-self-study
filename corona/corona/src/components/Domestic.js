0861b797bc989a3aa2a48a3a9cbbc86106d5429c
ChaMyungjun <acg70159@gmail.com>
2020-03-14 23:29:07 +0900

    Progress Domestic css

--- corona/corona/src/components/Domestic.js
+++ corona/corona/src/components/Domestic.js
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
-import '../public/css/Domestic.css'
+import React, { Component } from 'react';
 
 class Domestic extends Component {
   state = { users: [] };
 
   componentDidMount() {
-    fetch("/Domestic")
+    fetch('/Domestic')
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
 
   render() {
     return (
-
+      <div>
         <ul>
           {this.state.users.map(user => (
-            <div key={user.id} class = "accordion">
+            <div key={user.id} class="accordion">
               <h1>{user.title}</h1>
-              <div class = "accordion-item">
-                <div class = "item-header">
-                <h1>확진환자</h1>
-                <img class="icon" src="../public/img/plus"/>
+              <div class="accordion-item">
+                <div class="item-header">
+                  <h1>확진환자</h1>
+                  <img class="icon" src="../public/img/plus" />
                 </div>
-                <p class = "text">
-                {user.Confirm}명
-                </p>
+                <p class="text">{user.Confirm}명</p>
               </div>
               <p>{user.Disassociate}</p>
               <p>{user.Quaranines}</p>

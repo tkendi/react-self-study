48628b25ce1fdd45fca91335dbde77d04e526480
ChaMyungjun <acg70159@gmail.com>
2020-03-19 16:10:55 +0900

    change city design

--- corona/corona/src/components/City.js
+++ corona/corona/src/components/City.js
@@ -1,17 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 import '../public/css/City.css';
 
-class Corona extends Component {
-  constructor(props) {
-    super(props);
+const columns = [
+  { id: 'city', label: 'City', minwWidth: 170 },
+  { id: 'num', label: 'num', minWidth: 100 },
+  {
+    id: 'increase',
+    label: 'increase',
+    minWidth: 170,
+    align: 'right',
+    format: value => value.toLocaleString(),
+  },
+  {
+    id: 'dead',
+    label: 'Dead',
+    minWidth: 170,
+    align: 'right',
+    fomat: value => value.toLocaleString(),
+  },
+];
 
-    // this.customSouth = {
-    //   ...SouthKorea,
-    //   label: 'Custom map label',
-    //   location: SouthKorea.locations.map(location => {}),
-    // };
-  }
+function crateData(city, num, increase, dead) {
+  return { city, num, increase, dead };
+}
 
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
+
+class City extends Component {
   state = { datas: [] };
 
   componentDidMount() {
@@ -21,41 +53,63 @@ class Corona extends Component {
   }
 
   render() {
+    const classes = useStyles();
     return (
-      <div>
-        <ul>
-          {this.state.datas.map(data => (
-              <li key={data.id}>
-                <p>{data.Seoul_num}</p>
-              <p>{data.Seoul_Increase_num}</p>
-              <p>{data.Seoul_Dead_num}</p>
-              <p>{data.Busan_num}</p>
-              <p>{data.Busan_Increase_num}</p>
-              <p>{data.Busan_Dead_num}</p>
-              <p>{data.Daegu_num}</p>
-              <p>{data.Daegu_Increase_num}</p>
-              <p>{data.Daegu_Dead_num}</p>
-              <p>{data.Incheon_num}</p>
-              <p>{data.Incheon_Increase_num}</p>
-              <p>{data.Incheon_Dead_num}</p>
-              <p>{data.Gwangju_num}</p>
-              <p>{data.Gwangju_Increase_num}</p>
-              <p>{data.Gwangju_Dead_num}</p>
-              <p>{data.Daejeon_num}</p>
-              <p>{data.Daejeon_Increase_num}</p>
-              <p>{data.Daejeon_Dead_num}</p>
-              <p>{data.Ulsan_num}</p>
-              <p>{data.Ulsan_Increase_num}</p>
-              <p>{data.Ulsan_Dead_num}</p>
-              <p>{data.Sejong_num}</p>
-              <p>{data.Sejong_Increase_num}</p>
-              <p>{data.Sejong_Dead_num}</p>
-              </li>
-          ))}
-        </ul>
-      </div>
+      <Paper className={classes.root}>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map(column => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.users
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map(data => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tableIndex={-1}
+                      key={data.id}
+                    >
+                      {columns.map(column => {
+                        const value = data[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === 'number'
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={this.state.user.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
     );
   }
 }
 
-export default Corona;
+export default City;

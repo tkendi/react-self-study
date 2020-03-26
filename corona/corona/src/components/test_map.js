fee2dd82b1220c981c0922540d708ffc33042644
ChaMyungjun <acg70159@gmail.com>
2020-03-27 00:38:34 +0900

    cluster testing

--- corona/corona/src/components/test_map.js
+++ corona/corona/src/components/test_map.js
@@ -15,7 +15,7 @@ class test_map extends Component {
   //     level: 3, //지도의 확대 축소
   //   };
   //   this.map = new window.daum.maps.Map(container, options);
-    
+
   //   let markerPosition = new window.daum.maps.LatLng(Lat, Lng); //마커가 표시될 위치
   //   //마커 생성
   //   let marker = new window.daum.maps.Marker({
@@ -26,30 +26,27 @@ class test_map extends Component {
   // }
 
   componentDidMount() {
-
     let Lat = 37.5642135;
     let Lng = 127.0016985;
 
     let map = new window.kakao.maps.Map(document.getElementById('map'), {
       center: new window.kakao.maps.LatLng(Lat, Lng),
-      level: 14
+      level: 13, 
     });
 
     //마커 클러스터러 생성
     const cluster = new window.kakao.maps.MarkerClusterer({
       map: map, //마커들을 관리하고 표시할 지도
-      averageCenter: true,  //클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-      minLevel: 10  //클러스트 할 최소 지도 레벨
+      averageCenter: true, //클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+      minLevel: 10, //클러스트 할 최소 지도 레벨
     });
 
-    this.$.get('/download/web/data/chicken.json', function(data) {
-      let markers = this.$(data.position).map(function(i, position) {
-        return new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(position.lat, position.lng)
-        });
+    let markers = function(i, position) {
+      return new window.kakao.maps.Marker({
+        position: new window.kakao.maps.LatLng(Lat, Lng),
       });
-      cluster.addMarkers(markers);
-    })
+    };
+    cluster.addMarkers(markers);
   }
   render() {
     return <div className="test_map" id="map"></div>;

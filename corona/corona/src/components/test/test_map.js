234fc00bdfc1ac720ebe93e15ee2fb6271b02bdf
ChaMyungjun <acg70159@gmail.com>
2020-04-03 02:16:13 +0900

    testing jquery in react

--- corona/corona/src/components/test/test_map.js
+++ corona/corona/src/components/test/test_map.js
@@ -42,17 +42,27 @@ class test_map extends Component {
   // }
 
   componentDidMount() {
+    let container = document.getElementById('map')
+
     let Lat = 37.5642135;
     let Lng = 127.0016985;
 
-    let container = document.getElementById('map')
     let options = {
       center: new window.kakao.maps.LatLng(Lat, Lng),
       level: 14
     }
 
+
+
     let map = new window.kakao.maps.Map(container, options) 
-    let geocoder = new window.kakao.maps.services.Geocoder();
+    //let geocoder = new window.kakao.maps.services.Geocoder();
+
+    let clusterer = new window.kakao.maps.MarkerClusterer({
+      map: map,
+      averageCenter: true,
+      minLevel: 10
+    });
+
     // let clusterer = new window.kakao.maps.MarkerClusterer({
     //   map: map,
     //   averageCenter: true,
@@ -76,27 +86,17 @@ class test_map extends Component {
     //     )
     //   })   
     // })
-    geocoder.addressSearch('서울특별시 강남구 언주로211', (result, status) => {
-
-      //정상적으로 검색이 완료 되었을 때
-      if(status === window.kakao.maps.services.Status.OK) {
-        let coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-
-        //결과값으로 받은 위치를마커로 표시
-        let marker = new window.kakao.maps.Marker({
-          map: map,
-          position: coords
+    this.$.get('./test.json', (data) => {
+      //데이터에서 좌표 값을 가지고 마커를 표시한다
+      //마커 클러스터로 관리할 마커객체는 생성할 때 지도 설정
+      let markers = this.$(data.positions).map((i, position) => {
+        return new window.kakao.maps.Marker({
+          position: new window.kakao.maps.LatLng(position.lat, position.lng)
         });
+      });
 
-        var infowindow = new window.kakao.maps.InfoWindow({
-          content: '<div style="width:150px;text-align:center;padding:6px 0;">강남세브란스 병원</div>'
-        })
-        infowindow.open(map, marker);
-
-        map.setCenter(coords);
-      }
+      clusterer.addMarkers(markers);
     })
-
 
   }
   render() {

205fd72ae056e4d66117bcef080ce5ced44708a3
ChaMyungjun <acg70159@gmail.com>
2020-04-06 18:13:24 +0900

    world number parsing

--- covid_19/src/components/Cards/Cards.jsx
+++ covid_19/src/components/Cards/Cards.jsx
@@ -23,23 +23,37 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
                                 separator=","
                             />
                         </Typography>
-                        <Typography color = "textSecondary">REAL DATE</Typography>
+                        <Typography color = "textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant = "body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>Recovered</Typography>
-                        <Typography variant="h5">REAL DATA</Typography>
-                        <Typography color = "textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color = "textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant = "body2">Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>Deaths</Typography>
-                        <Typography variant="h5">REAL DATA</Typography>
-                        <Typography color = "textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color = "textSecondary">{new Date (lastUpdate).toDateString()}</Typography>
                         <Typography variant = "body2">Number of deaths caused by COVID-19</Typography>
                     </CardContent>
                 </Grid>

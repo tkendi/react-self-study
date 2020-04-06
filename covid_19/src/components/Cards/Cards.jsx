f0ae2114f583b15ff0620b1ef17ea90f314525e6
ChaMyungjun <acg70159@gmail.com>
2020-04-06 18:22:18 +0900

    card css

--- covid_19/src/components/Cards/Cards.jsx
+++ covid_19/src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 import styles from './Cards.module.css'
 
@@ -12,7 +13,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
     return (
         <div className = {styles.container}>
             <Grid container spacing = {3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className = {cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -27,7 +28,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
                         <Typography variant = "body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -42,7 +43,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
                         <Typography variant = "body2">Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">

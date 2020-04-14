73c71ca3de417a44786e799313605748c55b1385
ChaMyungjun <acg70159@gmail.com>
2020-04-15 01:11:35 +0900

    react_chat_study

--- chat-app/client/src/components/Chat/Chat.js
+++ chat-app/client/src/components/Chat/Chat.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
+import './Chat.css'
+
+import InfoBar from '../InfoBar/InfoBar'
+import Input from '../Input/Input'
+import Messages from '../Messages/Messages'
 
 let socket;
-
-import './Chat.css'
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
@@ -51,12 +54,9 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
-          <InfoBar />
-        {/* <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={event => event.key === "Enter" ? sendMessage(event) : null}
-        /> */}
+          <InfoBar room = {room}/>
+          <Messages messages={messages} name={name}/>
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
       </div>
     </div>
   );
--- chat-app/client/src/components/InfoBar/InfoBar.js
+++ chat-app/client/src/components/InfoBar/InfoBar.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 import './InfoBar.css';
 
-const InfoBar = () => {
-    <div className="InfoBar">
+import closeIcon from '../../icons/closeIcon.png'
+import onlineIcon from '../../icons/onlineIcon.png'
+
+const InfoBar = ({room}) => ( 
+    <div className="infoBar">
         <div className="leftInnerContainer">
             <img className="onlineIcon" src={onlineIcon} alt = "online image" />
-            <h3>roomName</h3>
+            <h3>{room}</h3>
         </div>
         <div className="RightInnerContainer">
             <a href="/"><img src={closeIcon} alt="close img"/></a>
         </div>
     </div>
-}
+)
 
 export default InfoBar;

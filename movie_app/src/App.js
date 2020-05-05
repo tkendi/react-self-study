bf1eac86c16e8f8ae8afd0cd3c0ecd57f3e0e68c
ChaMyungjun <acg70159@gmail.com>
2020-05-06 01:33:42 +0900

    movie_app study

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -5,48 +5,24 @@ import Movie from './Movie'
 class App extends Component {
 
   state = {
-    greeting: "Hello",
-    movies: [
-      {
-        title: "Matrix",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-      },
-      {
-        title: "FUll Metal Jacket",
-        poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-      },
-      {
-        title: "Star Wars",
-        poster: "https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372"
-      }
-    ]
+    
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          ...this.state.movies,
-          {
-            title: "Trainspotting",
-            poster: "https://i.pinimg.com/originals/ef/ac/43/efac437d6a42a6a2552048d81192c21a.jpg"
-          }
-        ]
-
-      })
-    }, 1000)
+    fetch('https://yts.ag/api/v2/list moives.json?sort by=rating')
   }
+
+_renderMovies = () => {
+  const movies = this.state.movies.map((movie, index) => {
+    return <Movie title = {movie.title} poster = {movie.poster} key = {index} />
+  })
+  return movies
+}
 
   render() {
     return (
       <div className = "App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title = {movie.title} poster = {movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     )
   }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -2,35 +2,28 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-    render() {
-        return(
-            <div>
-                <MoviePoster poster = {this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie ({title, poster}) {
+    return (
+        <div>
+            <MoviePoster poster = {poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
+function MoviePoster ({poster}) {
+    return (
+        <img src = {poster} alt="Movie Poster" />
+    )
+}
 
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
+Movie.propsTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
 
-    render() {
-        return(
-
-            // eslint-disable-next-line jsx-a11y/alt-text
-            <img src = {this.props.poster} alt="Movie Poster" />
-        ) 
-    }
+MoviePoster.propsTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie

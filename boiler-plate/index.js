1a35ee8b1f23f3c3006a9acb3d9d1f6b309def46
ChaMyungjun <acg70159@gmail.com>
2020-05-17 01:51:31 +0900

    bcypto sutdy

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,13 +3,16 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
 const {User} = require('./models/User')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb+srv://cha:june1209@boilerplate-rcw92.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Conntected...'))
   .catch(err => console.log(err))
@@ -23,12 +26,32 @@ app.post('/register', (req, res, next) => {
   //db에 저장한다
 
   const user = new User(req.body)
+
   user.save((err, doc) =>{
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
     })
   })
+})
+
+app.post('/login', (req, res) => {
+
+  //요청된 이메일을 데이터베이스 있는지 찾는다
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {return res.json({loginSuccess: false, message: '제공된 이메일에 해당하는 유저가 없습니다'})}
+  })
+  //요청된 이메일이 데이터 베이스 있다면 비밀번호가 맞는 비밀번호인지 확인
+
+  user.comparePassword( req.body.password, (err, isMatch) => {
+    if(!isMatch) {return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다"})}
+
+    //비밀번호가 맞다면 토큰까지 생성하기
+    user.generateToken((err, user) => {
+      
+    })
+  } )
+
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
---
+++ boiler-plate/models/User.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    //비밀번호 암호화
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        return next;
+      }
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+      next()
+  }
+});
+
+user.Schema.methods.comparePassword = function(plainPassword, cb) {
+
+
+    //plainPassword 1234567     암호화된 비밀번호 asklfjafjg와 같은지 체크
+    bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+        if(err) return cb(err),
+            cb(null, isMatch)
+    })
+
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };

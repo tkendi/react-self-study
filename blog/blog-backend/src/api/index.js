a87b622ed1f4cd0efad9825e2eaaf1c554521b01
ChaMyungjun <acg70159@gmail.com>
2020-05-19 18:12:53 +0900

    controller proceesing

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,9 +1,9 @@
 const Router = require('koa-router')
+const posts = require('./posts')
+
 const api = new Router()
 
-api.get('/test', ctx => {
-    ctx.body = 'test성공'
-})
+api.use('/posts', posts.routes())
 
 //라우터를 내보낸다
 module.exports = api;

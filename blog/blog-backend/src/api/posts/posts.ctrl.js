ba7c113a8ede5e06e423f7abe1a8590809a1e618
Tkendi <acg70159@gmail.com>
2020-06-01 00:22:19 +0900

    react setting

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,5 @@
 import Post from '../../models/post'
+import mongoose from 'mongoose'
 
 /**
  * POST /api/posts
@@ -24,17 +25,51 @@ export const write = async ctx => {
 }
 
 export const list = ctx => {
-
+    try{
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    } catch(e) {
+        ctx.throw(500, e)
+    } 
 }
 
 export const read = ctx => {
-
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();
+        if(!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch(e) {
+        ctx.throw(500, e)
+    }
 }
 
 export const remove = ctx => {
-
+    const { id } = ctx.params;
+    try{
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204;
+    } catch(e) {
+        ctx.throw(500, e)
+    }
 }
 
 export const update = ctx => {
-
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findByIdUpdate(id, ctx.request.body, {
+            new: true,      //이 값을 설정하면 업데이트된 데이터를 반환합니다
+            //false일 때 업데이트되기 전의 데이터를 반환한다
+        }).exec();
+        if(!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch(e) {
+        ctx.throw(500, e)
+    }
 }

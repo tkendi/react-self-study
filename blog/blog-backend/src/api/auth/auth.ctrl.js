076c936411b24adfa318757255f0a07bc8536b8d
Tkendi <acg70159@gmail.com>
2020-06-01 17:36:34 +0900

    api create

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -1,9 +1,76 @@
+import Joi from 'joi'
+import User from '../../models/user'
+
 export const register = async ctx => {
     //회원가입
+    const schema = Joi.object().keys({
+        username: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(20)
+            .required(),
+        password: Joi.string().required(),
+    });
+    const result = Joi.validate(ctx.request.body, schema);
+    if(result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
+    const {username, password} = ctx.request.body;
+    try{
+        //username이 이미 존재하는지 확인
+        const exists = await User.findByUsername(username);
+        if(exists) {
+            ctx.status = 409;   //Conflict
+            return;
+        }
+
+        const user = new User({
+            username,
+        })
+
+        await user.setPassword(password)
+        await user.save()
+
+        //응답할 데이터에서 hashedPassword 필드 제거
+        const data = user.toJSON();
+        delete data.hashedPassword;
+        ctx.body = user.serialize();
+    } catch(e) {
+        ctx.throw(500, e)
+    }
 }
 
 export const login = async ctx => {
     //로그인
+    const {username, password} = ctx.request.body;
+
+    //username, password가 없으면 예외처리
+    if(!username || !password) {
+        ctx.status = 401;   //Unauthorized
+        return
+    }
+
+    try {
+        const user = await User.findByUsername(username)
+        //계정이 존재하지 않으면 에러 처리
+        if(!user) {
+            ctx.status = 401
+            return
+        }
+
+        const valid = await user.checkPassword(password)
+        //잘못된 비밀번호
+        if(!valied) {
+            ctx.status = 401;
+            return
+        }
+        ctx.body = user.serialize()
+    } catch(e) {
+        ctx.throw(500, e)
+    }
 }
 
 export const check = async ctx => {

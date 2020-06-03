6acc0c078377560ca5da5b9e207991537e1563a5
Tkendi <acg70159@gmail.com>
2020-06-03 17:37:27 +0900

    login page error

--- final_project/src/auth/AuthForm.js
+++ final_project/src/auth/AuthForm.js
@@ -1,21 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import palette from '../styles/js/palette';
 
 const AuthFormBlock = styled.div`
-    border: 0px;
-    background: ${palette.cyan[6]};
-    
-`
+  border: 0px;
+  background: ${palette.cyan[6]};
+`;
 
 class AuthForm extends React.Component {
-    render() {
-        return (
-            <AuthForm>
-                AuthForm
-            </AuthForm>
-        )
-    }
+  render() {
+    return <AuthFormBlock>AuthForm</AuthFormBlock>;
+  }
 }
 
 export default AuthForm;
--- final_project/src/auth/AuthTemplate.js
+++ final_project/src/auth/AuthTemplate.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom'
-import palette from '../styles/js/palette'
-import Button from '../public_comp/Button'
+import { Link } from 'react-router-dom';
+import palette from '../styles/js/palette';
+import Button from '../public_comp/Button';
+import Box from '@material-ui/core';
 
-function AuthTemplate({children}) {
-    return (
-        <AuthTemplateBlock>
-            {children}
-        </AuthTemplateBlock>
-    )
+function AuthTemplate({ children }) {
+  return (
+    <div>
+      {children}
+      <Box component="span" m={1}>
+        <Button />
+      </Box>
+    </div>
+  );
 }
 
-export default AuthTemplate
+export default AuthTemplate;

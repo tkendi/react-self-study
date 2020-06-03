a7f5ef765f06bd8dab91baa10527c2fa4a2275ab
Tkendi <acg70159@gmail.com>
2020-06-03 17:08:48 +0900

    authform change

--- final_project/src/Web_page/Main.js
+++ final_project/src/Web_page/Main.js
@@ -5,7 +5,7 @@ class Main extends React.Component {
     render() {
         return (
             <div>
-                <Comp_Index />
+                
             </div>
         )
     }
--- final_project/src/auth/AuthForm.js
+++ final_project/src/auth/AuthForm.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import palette from '../styles/js/palette';
 
 const AuthFormBlock = styled.div`
-
+    border: 0px;
+    background: ${palette.cyan[6]};
+    
 `
 
 class AuthForm extends React.Component {

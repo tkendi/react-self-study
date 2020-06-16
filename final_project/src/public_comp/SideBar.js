209dce0521943458c338f42bdbfedec80ab909ac
Tkendi <acg70159@gmail.com>
2020-06-16 17:07:15 +0900

    text change

--- final_project/src/public_comp/SideBar.js
+++ final_project/src/public_comp/SideBar.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import { NavLink, BrowserRouter as Router, Route } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
-import '../styles/css/test_sidebar.scss'
+import '../styles/css/test_sidebar.scss';
+import { Typography } from '@material-ui/core';
 
 class test_sidebar extends Component {
   render() {
     return (
-      <Menu>
-          <NavLink className="menu-item" to="/">
-            Home
-          </NavLink>
+      <Menu width={250}>
+        <NavLink className="menu-item" to="/">
+          <Typography>Home</Typography>
+        </NavLink>
 
-          <NavLink className="menu-item"  to="/City">
-            Status City
-          </NavLink>
+        <NavLink className="menu-item" to="/City">
+          <Typography>Home</Typography>
+        </NavLink>
 
-          <NavLink className="menu-item" to="/Domestic"> 
-            Status Domestic
-          </NavLink>
+        <NavLink className="menu-item" to="/Domestic">
+          <Typography>Home</Typography>
+        </NavLink>
 
-          <NavLink className="menu-item" to="/test_map"> 
-            testing map
-          </NavLink>
+        <NavLink className="menu-item" to="/test_map">
+          <Typography>Home</Typography>
+        </NavLink>
       </Menu>
     );
   }
--- final_project/src/public_comp/testingSection.js
+++ final_project/src/public_comp/testingSection.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import { NavLink, BrowserRouter as Router, Route } from 'react-router-dom';
-import { slide as Menu } from 'react-burger-menu';
-import '../styles/css/test_sidebar.scss'
 
 class test_sidebar extends Component {
   render() {

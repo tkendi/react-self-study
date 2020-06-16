353823d523a531c584010422e58da15ff2fba0c3
Tkendi <acg70159@gmail.com>
2020-06-16 18:03:08 +0900

    sidebar change

--- final_project/src/public_comp/SideBar.js
+++ final_project/src/public_comp/SideBar.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import { NavLink, BrowserRouter as Router, Route } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
-import '../styles/css/test_sidebar.scss';
+import '../styles/css/SideBar.scss';
 import { Typography } from '@material-ui/core';
 
-class test_sidebar extends Component {
+class SideBar extends Component {
   render() {
     return (
       <Menu width={250}>
         <NavLink className="menu-item" to="/">
-          <Typography>Home</Typography>
+          <Typography variant = 'body2'>Home</Typography>
         </NavLink>
 
         <NavLink className="menu-item" to="/City">
-          <Typography>Home</Typography>
+          <Typography variant = 'body2'>Home</Typography>
         </NavLink>
 
         <NavLink className="menu-item" to="/Domestic">
-          <Typography>Home</Typography>
+          <Typography variant = 'body2'>Home</Typography>
         </NavLink>
 
         <NavLink className="menu-item" to="/test_map">
-          <Typography>Home</Typography>
+          <Typography variant = 'body2'>Home</Typography>
         </NavLink>
       </Menu>
     );
   }
 }
 
-export default test_sidebar;
+export default SideBar;

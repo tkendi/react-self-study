b7636e68dea1f8043f3f9266fffc2df1d76d4693
Tkendi <acg70159@gmail.com>
2020-06-19 23:30:03 +0900

    appbar change

--- final_project/src/public_comp/Appbar.js
+++ final_project/src/public_comp/Appbar.js
@@ -3,25 +3,25 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import SideBar from './SideBar';
-import {Button} from '@material-ui/core'
-import {Link} from 'react-router-dom'
-import '../styles/css/Appbar.scss'
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import '../styles/css/Appbar.scss';
 
-
-const Appbar = () => {
+const Appbar = ({history}) => {
   return (
     <div className="root">
       <SideBar />
-      <AppBar position="static" color = "secondary">
+      <AppBar position="static" color="secondary">
         <Toolbar>
           <Typography variant="h4" className="title">
             Animals Life
           </Typography>
-          <Button color = "inherit" onClick={() => alert('Click')}>Login</Button>
+          <Button color="inherit" onClick={() => {history.push('/login')}}>
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
   );
 };
-
-export default Appbar;
+export default Appbar;  

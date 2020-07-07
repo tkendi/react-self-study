3bf1d730710a36ca29b94c393dea5a2aae5ba1c7
Tkendi <acg70159@gmail.com>
2020-07-07 16:05:14 +0900

    Main change

--- final_project/front-end/src/Web_page/Main.js
+++ final_project/front-end/src/Web_page/Main.js
@@ -19,25 +19,14 @@ const theme = createMuiTheme({
       main: '#ffeb3b',
     },
   },
-  MuiButton: {
-    text: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      borderRadius: 3,
-      border: 0,
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    },
-  },
-  backImg: {
-    backgroundImage: 'url(https://source.unsplash.com/random)'
-  },
 });
 
 const useStyles = makeStyles((theme) => ({
   margin: {
     primary: theme.spacing(1),
+    width: '10rem',
+    height: '3rem',
+    alignItems: 'center'
   },
 }));
 
@@ -52,6 +41,7 @@ const Main = () => {
           <Button href = "/login">Sign IN</Button>
           <ThemeProvider theme={theme}>
             <Button
+              disableElevation
               variant="contained"
               color="primary"
               className={classes.margin}

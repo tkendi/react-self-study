9e3415021067a186e2b4c6c8adb45daa41c0813f
Tkendi <acg70159@gmail.com>
2020-07-11 23:23:38 +0900

    connectin mongodb success

--- final_project/back-end/index.js
+++ final_project/back-end/index.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+//비구조화 할당 process.env 내부의 값에 대한 래퍼런스
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connect to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const app = new Koa();
 const router = new Router();
@@ -16,6 +30,7 @@ app.use(bodyParser());
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening port ${port}`);
 });

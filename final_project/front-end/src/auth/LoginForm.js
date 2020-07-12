54c7cedbcbd6fbd1bbee9dd3a180010f2c9ff091
Tkendi <acg70159@gmail.com>
2020-07-12 15:50:09 +0900

    Loginform creating

---
+++ final_project/front-end/src/auth/LoginForm.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm } from '../modules/auth';
+import AuthForm from './AuthForm';
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const { form } = useSelector(({ auth }) => ({
+    form: auth.login,
+  }));
+
+  //input change handler
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: 'login',
+        key: name,
+        value,
+      })
+    );
+  };
+
+  //form event handler
+  const onSubmit = e => {
+      e.preventDefault()
+  }
+
+  //form initialize
+  useEffect(() => {
+    dispatch(initializeForm('login'));
+  }, [dispatch]);
+
+  return (
+    <AuthForm
+      type="login"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+};

080a51c9e10a9dd025ecf672575e1049801092d0
Tkendi <acg70159@gmail.com>
2020-07-15 09:33:16 +0900

    index change

--- delivery/svr/src/index.js
+++ delivery/svr/src/index.js
@@ -4,12 +4,11 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx, next) => {
-    ctx.body = 'Root Page'
-})
-
-router.get('/sub', (ctx, next) => {
-    ctx.body = 'sub page'
+router.get('/delivery', (ctx, next) => {
+    const {number} = ctx.query;
+    //number유무에 따라 출력결과가 다르다
+    if(length(number.toString()) > )
+    console.log(number)
 })
 
 app.use(router.routes())

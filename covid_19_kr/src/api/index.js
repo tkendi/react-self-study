1cc3102d671a9979500e81f461b0789ae42addd7
Tkendi <acg70159@gmail.com>
2020-07-19 03:26:32 +0900

    time compare

--- covid_19_kr/src/api/index.js
+++ covid_19_kr/src/api/index.js
@@ -1,21 +1,32 @@
 import axios from 'axios'
 
 const date = new Date()
-const startDt = `${date.getFullYear()}${("0" + (date.getMonth() - 1)).slice(-2)}${("0" + date.getDate()).slice(-2)}`
-const endDt = `${date.getFullYear()}${("0" + (date.getMonth() - 1)).slice(-2)}${("0" + date.getDate()).slice(-2)}`
+const hours = date.getHours();
+let startDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + (date.getDate())).slice(-2)}`
+let endDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + (date.getDate())).slice(-2)}`
+
+if (hours < 7) {
+    startDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + (date.getDate() - 1)).slice(-2)}`
+    endDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + (date.getDate() - 1)).slice(-2)}`
+}
 
 const url = `/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=${process.env.REACT_APP_serviceKey}&pageNo=1&numOfRows=10&startCreateDt=${startDt}&endCreateDt=${endDt}&_type=json`
 
 export const fetchData = async() => {
     try {
         const data = await axios.get(url)
-        const items = data.data.response.body.items
-        const {city, daily_confirmed} = {}
+        const items = data.data.response.body.items.item
+        const city = {}
+
+        city.startDt = data.data.response.body.items.item[18].stdDay
+        city.endDt = city.startDt
 
         for (const keys in items) {
-            city[items[keys].title] = items[keys].gubun
-            console.log(city)
+            city[items[keys].gubun] = items[keys].incDec
         }
+
+        console.log(city)
+
         return items
     } catch(e) {
         console.log(e)

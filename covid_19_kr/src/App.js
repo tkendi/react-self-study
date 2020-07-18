e5587e8c1f64b835e0fda3b5ce15738070ba9511
Tkendi <acg70159@gmail.com>
2020-07-18 21:32:19 +0900

    github modify

--- covid_19_kr/src/App.js
+++ covid_19_kr/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { fetchData } from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    console.log(fetchedData);
+  }
+
+  render() {
+    return <h1>testing</h1>;
+  }
 }
 
 export default App;
---
+++ covid_19_kr/src/api/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const date = new Date()
+const startDt = `${date.getFullYear()}${("0" + (date.getMonth() - 1)).slice(-2)}${("0" + date.getDate()).slice(-2)}`
+const endDt = `${date.getFullYear()}${("0" + (date.getMonth() - 1)).slice(-2)}${("0" + date.getDate()).slice(-2)}`
+
+const url = `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=${process.env.REACT_APP_serviceKey}&pageNo=1&numOfRows=10&startCreateDt=${startDt}&endCreateDt=${endDt}&_type=json`
+
+export const fetchData = async() => {
+    try {
+        const data = await axios.get(url)
+        .then(function (res) {
+            console.log(res);
+        })
+
+        return data;
+    } catch(e) {
+        console.log(e)
+    }
+}

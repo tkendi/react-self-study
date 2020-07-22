4297220efd5c113126588c9662f8d24544038e2b
Tkendi <acg70159@gmail.com>
2020-07-22 12:38:08 +0900

    code rebuilding

--- covid_19_kr/src/api/index.js
+++ covid_19_kr/src/api/index.js
@@ -1,46 +1,54 @@
-import axios from 'axios'
+import axios from "axios";
 
-const date = new Date()
+const date = new Date();
 const hours = date.getHours();
-let startDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + (date.getDate())).slice(-2)}`
-let endDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + (date.getDate())).slice(-2)}`
+let startDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(
+  -2
+)}${("0" + date.getDate()).slice(-2)}`;
+let endDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${(
+  "0" + date.getDate()
+).slice(-2)}`;
 
-if (hours < 10) {
-    startDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + (date.getDate() - 1)).slice(-2)}`
-    endDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + (date.getDate() - 1)).slice(-2)}`
+if (hours < 9) {
+  startDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${(
+    "0" +
+    (date.getDate() - 1)
+  ).slice(-2)}`;
+  endDt = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${(
+    "0" +
+    (date.getDate() - 1)
+  ).slice(-2)}`;
 }
 
-const url = `/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=${process.env.REACT_APP_serviceKey}&pageNo=1&numOfRows=10&startCreateDt=${startDt}&endCreateDt=${endDt}&_type=json`
+const url = `/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=${process.env.REACT_APP_serviceKey}&pageNo=1&numOfRows=10&startCreateDt=${startDt}&endCreateDt=${endDt}&_type=json`;
 
-export const cityPicker = async() => {
-    try {
-        const data = await axios.get(url)
-        const items = data.data.response.body.items.item
-        const city = []
+export const cityPicker = async () => {
+  try {
+    const data = await axios.get(url);
+    const items = data.data.response.body.items.item;
+    const city = [];
 
-        console.log(data)
+    console.log(data);
 
-        for (const keys in items) {
-            city[keys] = items[keys].gubun
-        }
-
-        return Array.from(city)
-    } catch(e) {
-        console.log(e)
+    for (const keys in items) {
+      city[keys] = items[keys].gubun;
     }
-}   
 
-export const numbers = async(city) => {
-    try {
-        const data = await axios.get(url);
-        const items = data.data.response.body.items.item
-        let numbers = 0
+    return Array.from(city);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-        for (const keys in items) {
-            numbers = items[city].incDec
-        }
-        return numbers
-    } catch(e) {
-        console.log(e)
-    }
-}
+export const numbers = async (city) => {
+  try {
+    const data = await axios.get(url);
+    const items = data.data.response.body.items.item;
+    let numbers = 0;
+
+    numbers = items[city].incDec;
+    return numbers;
+  } catch (e) {
+    console.log(e);
+  }
+};

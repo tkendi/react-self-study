76c6b3e2af7d22f237431b1a7e56ac9d09776b34
Tkendi <acg70159@gmail.com>
2020-07-23 11:33:28 +0900

    cj parsing

--- delivery/svr/src/index.js
+++ delivery/svr/src/index.js
@@ -18,18 +18,23 @@ router.get("/delivery", (ctx, next) => {
     axios.get(`${URL}/${number}`)
     .then(function(res) {
       const progresses = res.data.progresses
-      let number = {}
-      let time = {}
-      let description = {}
+
+      const time = {}
+      const location = {}
+      const description = {}
 
       console.log(res.data.progresses)
       for(const keys in progresses) {
+        time[keys] = progresses[keys].time
+        location[keys] = progresses[keys].location
+        description[keys] = progresses[keys].description
+
         console.log(progresses[keys].time)
         console.log(progresses[keys].location)
         console.log(progresses[keys].description)
       }
+      return ({time, location, description})
     })
-    console.log( time, location, description )
   } else {
     console.log("failure");
   }

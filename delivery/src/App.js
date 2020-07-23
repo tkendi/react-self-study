de0d05c4355ea542a24f4697f7db69856d61e269
Tkendi <acg70159@gmail.com>
2020-07-23 18:18:32 +0900

    parsing moduling

--- delivery/src/App.js
+++ delivery/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import axios from 'axios'
 
 class App extends React.Component {
-  componentDidMount() {
-    fetch("http://localhost:4000/api")
-      .then((res) => res.json())
-      .then((data) => this.setState({ username: data.username }));
+    async  componentDidMount() {
+        const test = await axios('http://localhost:4000/delivery?number=632234576584')
+        console.log(test)
   }
   render() {
       const username = this.state;
---
+++ delivery/src/lib/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const cj_parsing = () => {
+    cosnt 
+}

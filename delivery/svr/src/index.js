9e08527fca253bf586e143a3bd0aeb1fd2c0f5c0
Tkendi <acg70159@gmail.com>
2020-07-23 15:12:16 +0900

    code rebuilding

--- delivery/svr/src/index.js
+++ delivery/svr/src/index.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const Koa = require("koa");
-const Router = require("koa-router");
-const bodyParser = require('koa-bodyparser')
-const cheerio = require("cheerio");
-const axios = require("axios")
+const Router = require('koa-router')
+const axios = require("axios");
 
 const app = new Koa();
 const router = new Router();
@@ -14,36 +12,32 @@ const { URL, PORT } = process.env;
 
 router.get("/delivery", (ctx, next) => {
   const { number } = ctx.query;
+
+  const time = {};
+  const location = {};
+  const description = {};
+
   if (String(number).length >= 11 || String(number).length >= 12) {
-    axios.get(`${URL}/${number}`)
-    .then(function(res) {
-      const progresses = res.data.progresses
+    axios.get(`${URL}/${number}`).then(function (res) {
+      const progresses = res.data.progresses;
+      console.log(res.data.progresses);
+      for (const keys in progresses) {
+        time[keys] = progresses[keys].time;
+        location[keys] = progresses[keys].location;
+        description[keys] = progresses[keys].description;
 
-      const time = {}
-      const location = {}
-      const description = {}
-
-      console.log(res.data.progresses)
-      for(const keys in progresses) {
-        time[keys] = progresses[keys].time
-        location[keys] = progresses[keys].location
-        description[keys] = progresses[keys].description
-
-        console.log(progresses[keys].time)
-        console.log(progresses[keys].location)
-        console.log(progresses[keys].description)
+        console.log(progresses[keys].time);
+        console.log(progresses[keys].location);
+        console.log(progresses[keys].description);
       }
-      return({time, location, description})
-    })
+    });
   } else {
     console.log("failure");
   }
   console.log(number);
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.use(bodyParser())
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, () => {
   console.log("Koa server is listening to port 4000");

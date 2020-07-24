85a059f281cac8a8bd69f7b90b31698eff17860d
Tkendi <acg70159@gmail.com>
2020-07-24 20:48:28 +0900

    state initialize

--- delivery/src/App.js
+++ delivery/src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import {cjParsing} from './lib/index'
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state({
+      time: [],
+      location: [],
+      description: []
+    })
+  }
+
     async componentDidMount() {
       const data = await cjParsing();
-      const time = data.time
-      const location = data.location
-      const description = data.description
-
-      console.log(time, location, description)
+      this.setState({
+      time: data.time,
+      location: data.location,
+      description: data.description
+    })
+      console.log(this.state)
     }
   render() {
     return (

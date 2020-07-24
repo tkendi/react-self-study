c940ac2bbbc1494f84752c77603d6d187c7ed943
Tkendi <acg70159@gmail.com>
2020-07-24 16:42:35 +0900

    react axios parsing

--- delivery/src/lib/index.js
+++ delivery/src/lib/index.js
@@ -6,7 +6,21 @@ export const cjParsing = async() => {
   try {
     const data = await axios.get(`${url}/632234576584`)
     console.log(data)
+    const time = []
+    const location = []
+    const description = []
+
+    const progress = data.data.progresses
+    console.log(progress)
+    for (const keys in progress) {
+      time[keys] = progress[keys].time,
+      location[keys] = progress[keys].location,
+      description[keys] = progress[keys].description
+    }
+
+    console.log(time)
+
   } catch(e) {
     console.log(e)
   }
-} 
+}

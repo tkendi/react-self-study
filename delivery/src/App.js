bf91f6cb7a3b17ede363a432652dc4fc64d0dd14
Tkendi <acg70159@gmail.com>
2020-07-24 20:41:25 +0900

    react axios parsing

--- delivery/src/App.js
+++ delivery/src/App.js
@@ -4,7 +4,11 @@ import {cjParsing} from './lib/index'
 class App extends React.Component {
     async componentDidMount() {
       const data = await cjParsing();
-      console.log(data)
+      const time = data.time
+      const location = data.location
+      const description = data.description
+
+      console.log(time, location, description)
     }
   render() {
     return (
--- delivery/src/lib/index.js
+++ delivery/src/lib/index.js
@@ -1,26 +1,27 @@
-import axios from 'axios'
+import axios from "axios";
 
-const url = process.env.REACT_APP_URL
+const url = process.env.REACT_APP_URL;
 
-export const cjParsing = async() => {
+export const cjParsing = async () => {
   try {
-    const data = await axios.get(`${url}/632234576584`)
-    console.log(data)
-    const time = []
-    const location = []
-    const description = []
+    const data = await axios.get(`${url}/632234576584`);
+    console.log(data);
+    const parsing = {
+      time: [],
+      location: [],
+      description: [],
+    };
 
-    const progress = data.data.progresses
-    console.log(progress)
+    const progress = data.data.progresses;
+    console.log(progress);
     for (const keys in progress) {
-      time[keys] = progress[keys].time,
-      location[keys] = progress[keys].location,
-      description[keys] = progress[keys].description
+      parsing.time[keys] = progress[keys].time
+      parsing.location[keys] = progress[keys].location.name
+      parsing.description[keys] = progress[keys].description
     }
 
-    console.log(time)
-
-  } catch(e) {
-    console.log(e)
+    return parsing
+  } catch (e) {
+    console.log(e);
   }
-}
+};

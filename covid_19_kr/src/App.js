9e943703825aef3bbe71700aa57ac91eb9737488
Tkendi <acg70159@gmail.com>
2020-07-27 12:10:46 +0900

    style change

--- covid_19_kr/src/App.js
+++ covid_19_kr/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import City from "./components/city";
 import Number from "./components/numberCard";
 import styles from "./styles/App.module.css";
+import { AppBar, Toolbar, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 class App extends React.Component {
   state = {
@@ -17,11 +19,21 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className={styles.form}>
-        <h1>COVID-19</h1>
-        <City handleCityChange={this.handleCityChange} />
-        <Number city={this.state.data} />
-      </div>
+      <React.Fragment>
+        <div className={styles.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" color="inherit" aria-label="open drawer">
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <div className={styles.form}>
+          <City handleCityChange={this.handleCityChange} />
+          <Number city={this.state.data} />
+        </div>
+      </React.Fragment>
     );
   }
 }

25dbbc3a6cbbbe4e56d27743c00703e53e00c0cc
Tkendi <acg70159@gmail.com>
2020-07-27 21:53:56 +0900

    style change

--- delivery/src/components/cjParsing.js
+++ delivery/src/components/cjParsing.js
@@ -8,7 +8,9 @@ import {
   TableCell,
   TableBody,
   TableRow,
+  Paper,
 } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
 import styles from "../styles/cj.module.css";
 
 class cjParsing extends React.Component {
@@ -22,8 +24,8 @@ class cjParsing extends React.Component {
   async componentDidUpdate(prevProps, prevState) {
     if (prevProps.number !== this.props.number) {
       const data = await cjDataParsing(this.props.number);
-      if(!data) {
-        return '잘못된 접근입니다'
+      if (!data) {
+        return "잘못된 접근입니다";
       }
       this.setState({
         time: data.time,
@@ -37,32 +39,33 @@ class cjParsing extends React.Component {
   render() {
     const { time, location, description } = this.state;
     return (
-      <div className={styles.form}>
+      <Paper className = {styles.form}>
         <TableContainer>
           <Table
             className={styles.table}
-            size = "small"
+            size="small"
             aria-label="delivery-info"
+            className = {styles.table}
           >
             <TableHead>
               <TableRow className={styles.tableRow}>
                 <TableCell>
-                  <Typography align="center" variant="subtitle1" component="h3">
+                  <Typography variant="subtitle1" component="h3">
                     상품단계
                   </Typography>
                 </TableCell>
-                <TableCell align="right">
-                  <Typography align="center" variant="subtitle1" component="h3">
+                <TableCell align="center">
+                  <Typography variant="subtitle1" component="h3">
                     처리일시
                   </Typography>
                 </TableCell>
-                <TableCell align="right">
-                  <Typography align="center" variant="subtitle1" component="h3">
+                <TableCell align="center">
+                  <Typography variant="subtitle1" component="h3">
                     상품상태
                   </Typography>
                 </TableCell>
-                <TableCell align="right">
-                  <Typography align="center" variant="subtitle1" component="h3">
+                <TableCell align="center">
+                  <Typography variant="subtitle1" component="h3">
                     상품위치
                   </Typography>
                 </TableCell>
@@ -75,15 +78,15 @@ class cjParsing extends React.Component {
                   <TableCell component="th" scopre="row">
                     {this.state.state}
                   </TableCell>
-                  <TableCell align="left">{time}</TableCell>
-                  <TableCell align="left">{description[i]}</TableCell>
-                  <TableCell align="left">{location[i]}</TableCell>
+                  <TableCell align="center">{time}</TableCell>
+                  <TableCell align="center">{description[i]}</TableCell>
+                  <TableCell align="center">{location[i]}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-      </div>
+      </Paper>
     );
   }
 }

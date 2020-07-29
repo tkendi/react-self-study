ed8e91507a435b1ca4f6876ccb2dc09db1aa95a8
Tkendi <acg70159@gmail.com>
2020-07-30 01:13:43 +0900

    server react connection

--- waether-forecasting/svr/server.js
+++ waether-forecasting/svr/server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express();
 const api = require('./routes/index');
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const PORT = 4000;
 
-app.use('/api', api)
 app.use(cors())
+app.use('/api', api)
 
 app.listen(PORT, () => {
     console.log(`Listening port ${PORT}`)

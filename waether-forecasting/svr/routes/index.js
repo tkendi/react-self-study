bca47e68fbc68a6fd97028abe83a887b4f054f9d
Tkendi <acg70159@gmail.com>
2020-07-30 13:29:32 +0900

    data save

--- waether-forecasting/svr/routes/index.js
+++ waether-forecasting/svr/routes/index.js
@@ -1,20 +1,41 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const fs = require('fs')
-const axios = require('axios')
+const fs = require("fs");
+const axios = require("axios");
+const { info } = require("console");
 
-const URL = process.env.URL
-const date = new Date()
+const url = process.env.URL;
+const date = new Date();
 
-const base_Date = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + date.getDate()).slice(-2)}`
-const base_Time = `${("0" + date.getHours()).slice(-2)}00`
+const saveFile = (info) => {
+    fs.writeFileSync('../data/data.json', JSON.stringify(info))
+}
 
+date.setMinutes(date.getMinutes() - 40);
 
-router.get('/', async (req, res) => {
-    const data = await axios.get(`${URL}&serviceKey=${process.env.serviceKey}&numOfRows=10&pageNo=1&numOfRows=10&dataType=json&base_date=${base_Date}&base_time=${base_Time}&nx=60&ny=127`)
-    //fs.writeFileSync('../data/data.json', data)
-    console.log(data.data)
-})
+const base_Date = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(
+  -2
+)}${("0" + date.getDate()).slice(-2)}`;
+const base_Time = `${("0" + date.getHours()).slice(-2)}00`;
+
+router.get("/", async (req, res) => {
+  const info = {};
+  const data = await axios.get(
+    `${url}?serviceKey=${process.env.serviceKey}&numOfRows=10&pageNo=1&dataType=json&base_date=${base_Date}&base_time=${base_Time}&nx=60&ny=127`
+  );
+  const items = data.data.response.body.items.item;
+
+  info.baseDate = items[0].baseDate;
+  info.bsaeTime = items[0].baseTime;
+
+  for (const keys in items) {
+    info[items[keys].category] = items[keys].obsrValue;
+  }
+
+  saveFile(info)
+  console.log(info);
+  res.send(data.data);
+});
 
 module.exports = router;

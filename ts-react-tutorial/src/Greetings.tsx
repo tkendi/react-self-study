f70d59b5309ba38ebf16b184ab880894ad89a835
Tkendi <acg70159@gmail.com>
2020-07-30 22:16:13 +0900

    study react typescript

--- ts-react-tutorial/src/Greetings.tsx
+++ ts-react-tutorial/src/Greetings.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
-type GreetingsProps = {
-    name: string;
+type GreetingProps = {
+  name: string;
+  mark: string;
+  optional?: string;
+  onClick: (name: string) => void;
+};
+
+function Greetings({ name, mark, optional, onClick }: GreetingProps) {
+  const handleClick = () => onClick(name);
+  return (
+    <div>
+      Hello, {name} {mark}
+      {optional && <p>{optional}</p>}
+      <div>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+    </div>
+  );
 }
 
-const Greetings: React.FC<GreetingsProps> = ({name}) => (
-    <div>Hello, {name}</div>
-)
+Greetings.defaultProps = {
+  mark: "!",
+};
 
-export default Greetings
+export default Greetings;

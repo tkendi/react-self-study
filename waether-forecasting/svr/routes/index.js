58fc2dfe444932924894f7256d00b3cfa2f39c11
Tkendi <acg70159@gmail.com>
2020-07-30 11:50:13 +0900

    weather parsing

--- waether-forecasting/svr/routes/index.js
+++ waether-forecasting/svr/routes/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const router = express.Router();
 const fs = require('fs')
@@ -9,9 +10,11 @@ const date = new Date()
 const base_Date = `${date.getFullYear()}${("0" + (date.getMonth() + 1)).slice(-2)}${("0" + date.getDate()).slice(-2)}`
 const base_Time = `${("0" + date.getHours()).slice(-2)}00`
 
+
 router.get('/', async (req, res) => {
-    const data = await axios.get(`${URL}&serviceKey=${process.env.serviceKey}&numOfRows=10&pageNo=1&dataType=json&base_date=${base_Date}&base_time=${base_Time}&nx=37&ny=127`)
-    fs.writeFileSync('../data/data.json', data)
+    const data = await axios.get(`${URL}&serviceKey=${process.env.serviceKey}&numOfRows=10&pageNo=1&numOfRows=10&dataType=json&base_date=${base_Date}&base_time=${base_Time}&nx=60&ny=127`)
+    //fs.writeFileSync('../data/data.json', data)
+    console.log(data.data)
 })
 
 module.exports = router;

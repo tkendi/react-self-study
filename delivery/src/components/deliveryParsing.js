043534c82a4a8764589faf9b09f824d25e26a0f1
Tkendi <acg70159@gmail.com>
2020-08-01 20:25:54 +0900

    style change

--- delivery/src/components/deliveryParsing.js
+++ delivery/src/components/deliveryParsing.js
@@ -38,60 +38,61 @@ class deliveryParsing extends React.Component {
     if (prevProps.number !== this.props.number) {
       console.log(this.props.number);
       const parsing = await deliverDataParsing(this.state.url, this.props.number);
-      console.log(parsing);
+      this.setState({
+        time: parsing.time,
+        description: parsing.description,
+        location: parsing.location,
+        state: parsing.state
+      })
     }
   }
 
   render() {
     const { time, location, description } = this.state;
     return (
-      // <Paper className={styles.form}>
-      //   <TableContainer>
-      //     <Table size="small" aria-label="delivery-info" padding="default">
-      //       <TableHead>
-      //         <TableRow>
-      //           <TableCell>
-      //             <Typography variant="subtitle1" component="h3">
-      //               상품단계
-      //             </Typography>
-      //           </TableCell>
-      //           <TableCell align="center">
-      //             <Typography variant="subtitle1" component="h3">
-      //               처리일시
-      //             </Typography>
-      //           </TableCell>
-      //           <TableCell align="center">
-      //             <Typography variant="subtitle1" component="h3">
-      //               상품상태
-      //             </Typography>
-      //           </TableCell>
-      //           <TableCell align="center">
-      //             <Typography variant="subtitle1" component="h3">
-      //               상품위치
-      //             </Typography>
-      //           </TableCell>
-      //         </TableRow>
-      //       </TableHead>
+      <Paper className={styles.form}>
+        <TableContainer>
+          <Table size="small" aria-label="delivery-info" padding="default">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="subtitle1" component="h3">
+                    상품단계
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="subtitle1" component="h3">
+                    처리일시
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="subtitle1" component="h3">
+                    상품상태
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="subtitle1" component="h3">
+                    상품위치
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
 
-      //       <TableBody>
-      //         {time.map((time, i) => (
-      //           <TableRow key={i}>
-      //             <TableCell component="th" scopre="row">
-      //               {this.state.state}
-      //             </TableCell>
-      //             <TableCell align="center">{time}</TableCell>
-      //             <TableCell align="center">{description[i]}</TableCell>
-      //             <TableCell align="center">{location[i]}</TableCell>
-      //           </TableRow>
-      //         ))}
-      //       </TableBody>
-      //     </Table>
-      //   </TableContainer>
-      // </Paper>
-
-      <div>
-        <Typography>App</Typography>
-      </div>
+            <TableBody>
+              {time.map((time, i) => (
+                <TableRow key={i}>
+                  <TableCell component="th" scopre="row">
+                    {this.state.state}
+                  </TableCell>
+                  <TableCell align="center">{time}</TableCell>
+                  <TableCell align="center">{description[i]}</TableCell>
+                  <TableCell align="center">{location[i]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     );
   }
 }

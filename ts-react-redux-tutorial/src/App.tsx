1e4efcd6283ee95277b14ede8abbf6fce5de202c
Tkendi <acg70159@gmail.com>
2020-08-02 12:32:42 +0900

    hook pattern

--- ts-react-redux-tutorial/src/App.tsx
+++ ts-react-redux-tutorial/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import CounterContainer from "./containers/CounterContainer";
+import Counter from "./components/Counter";
 
 function App() {
-  return <CounterContainer />;
+  return <Counter />;
 }
 
 export default App;

9eba9f447f64ad1d9bfa4c99fb30d71b9a55acfd
Tkendi <acg70159@gmail.com>
2020-08-03 19:21:31 +0900

    weather parsing

--- weather-forecasting/src/App.js
+++ weather-forecasting/src/App.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { weatherData } from "./api";
 import City from "./components/CityPicker/cityPicker";
 
 const App = () => {
-  const [number, setNumber] = useState(0);
-  useEffect(async () => {
-    const data = await weatherData(98, 76)
-    console.log(data);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await weatherData(60, 127)
+      console.log(data)
+    }
+    fetchData()
     return () => {
       console.log("I'm dying...");
     };
-  }, [number]);
+  }, []);
   return (
     <div>
-      <h2>number is {number}</h2>
-      <button onClick={() => setNumber(number + 1)}>Increment</button>
+      <City />
     </div>
   );
 };
--- weather-forecasting/src/components/CityPicker/cityPicker.js
+++ weather-forecasting/src/components/CityPicker/cityPicker.js
@@ -1,18 +1,41 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, TextField, MenuItem } from "@material-ui/core";
 
 const cityPciker = () => {
+  const city = {
+    city: [
+      "Seoul",
+      "Busan",
+      "Daegu",
+      "Incheon",
+      "Gwangu",
+      "Daejeon",
+      "Ulsan",
+      "Jeju",
+    ],
+    nx: ["60", "98", "89", "55", "58", "67", "102", "52"],
+    ny: ["127", "76", "90", "124", "74", "100", "84", "38"],
+  };
 
-    const city = {
-        city: ["Seoul", "Busan", "Daegu", "Incheon", "Gwangu", "Daejeon", "Ulsan", "Jeju"],
-        nx: ["60", "98", "89", "55", "58", "67", "102", "52"], 
-        ny: ["127", "76", "90", "124", "74", "100", "84", "38"]
-    }
-
-    return (
-    <div>
-      <Typography>CityPicker</Typography>
-    </div>
+  return (
+    <React.Fragment>
+      <Typography>
+        <TextField
+          select
+          label="City"
+          helperText="List of City"
+          onChange={(e) => console.log(e.target.value)}
+        >
+          {city.city.map((city, index) => {
+            return (
+              <MenuItem key={index} value={city}>
+                {city}
+              </MenuItem>
+            );
+          })}
+        </TextField>
+      </Typography>
+    </React.Fragment>
   );
 };
 

e8dffd1bf172b132ed5e8fb5d12f27b22befc64d
Tkendi <acg70159@gmail.com>
2020-08-04 00:59:33 +0900

    page seperate

--- weather-forecasting/src/App.js
+++ weather-forecasting/src/App.js
@@ -1,30 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { weatherData } from "./api";
 import City from "./components/CityPicker/cityPicker";
-import { Typography } from "@material-ui/core";
+
+import WeatherMain from "./components/Page/main/main";
 
 const App = () => {
   const [nx, setNx] = useState(0);
   const [ny, setNy] = useState(0);
-  const [temp, setTemp] = useState(0);
-  const [rain, setRain] = useState(0);
-  const [type, setType] = useState(0);
-  const [baseTime, setBaseTime] = useState(0);
 
-  useEffect(() => {
-    async function fetchData() {
-      const data = await weatherData(nx, ny);
-      setBaseTime(data.baseTime);
-      setTemp(data.TMP);
-      setRain(data.RNH);
-      setType(data.SKY);
-      console.log(data);
-    }
-    fetchData();
-    return () => {
-      console.log("I'm dying...");
-    };
-  }, [nx, ny]);
+
 
   const cityCode = (cityNx, cityNy) => {
     setNx(cityNx);
@@ -36,20 +19,8 @@ const App = () => {
   return (
     <div>
       <City cityParsing={cityCode} />
-      <Typography>{baseTime}</Typography>
-      <Typography>{temp}&deg;C</Typography>
-      <Typography>{type}</Typography>
-      {type === 1 ? (
-        <Typography>Rain</Typography>
-      ) : type === 2 ? (
-        <Typography>Rain/Snow</Typography>
-      ) : type === 3 ? (
-        <Typography>Snow</Typography>
-      ) : type == 4 ? (
-        <Typography>Shower</Typography>
-      ) : (
-        <Typography>clear</Typography>
-      )}
+      <WeatherMain nx={nx} ny={ny} />
+
     </div>
   );
 };
---
+++ weather-forecasting/src/components/Page/main/main.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import clear from "./img/sun.png";
+import snow from "./img/snow.png";
+import shower from "./img/meteor-shower.png";
+import ice from "./img/ice.png";
+import rainy from "./img/rain.png";
+import { weatherData } from "../../../api";
+
+const main = ({ nx, ny }) => {
+  const [temp, setTemp] = useState(0);
+  const [rain, setRain] = useState(0);
+  const [type, setType] = useState(0);
+  const [baseTime, setBaseTime] = useState(0);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await weatherData(nx, ny);
+      setBaseTime(data.baseTime);
+      setTemp(data.TMP);
+      setRain(data.RNH);
+      setType(data.SKY);
+      console.log(data);
+    }
+    fetchData();
+    return () => {
+      console.log("I'm dying...");
+    };
+  }, [nx, ny]);
+
+  return (
+    <React.Fragment>
+      <Typography>{baseTime}</Typography>
+      <Typography>{temp}&deg;C</Typography>
+      <Typography>{RNH}</Typography>
+      {type === 1 ? (
+        <img src={rainy} alt="rain" />
+      ) : type === 2 ? (
+        <img src={ice} alt="ice" />
+      ) : type === 3 ? (
+        <img src={snow} alt="snow" />
+      ) : type === 4 ? (
+        <img src={shower} alt="shower" />
+      ) : (
+        <img src={clear} alt="clear" />
+      )}
+    </React.Fragment>
+  );
+};

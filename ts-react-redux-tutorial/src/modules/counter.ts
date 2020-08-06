97e27f9af9a379c846ff0156edbb20a600a6aa7d
Tkendi <acg70159@gmail.com>
2020-08-06 14:03:06 +0900

    study react typescript

--- ts-react-redux-tutorial/src/modules/counter.ts
+++ ts-react-redux-tutorial/src/modules/counter.ts
@@ -1,29 +1,22 @@
-import {createAction, ActionType, createReducer} from 'typesafe-actions'
+import { createAction, createReducer } from 'typesafe-actions';
 
-
-const INCREASE = "counter/INCREASE";
-const DECREASE = "counter/DECREASE";
-const INCREASE_BY = "counter/INCREASE_BY";
-
-export const increase = createAction(INCREASE)();
-export const decrease = createAction(DECREASE)()
-export const increaseBy = createAction(INCREASE_BY)<number>();
-
-const actions = {increase, decrease, increaseBy}
-type CounterAction = ActionType<typeof actions>;
+export const increase = createAction('counter/INCREASE')();
+export const decrease = createAction('counter/DECREASE')();
+export const increaseBy = createAction('counter/INCREASE_BY')<number>(); // payload 타입을 Generics 로 설정해주세요.
 
 type CounterState = {
   count: number;
 };
 
 const initialState: CounterState = {
-  count: 0,
+  count: 0
 };
 
-const counter = createReducer<CounterState, CounterAction>(initialState, {
-  [INCREASE]: state => ({count: state.count + 1}),
-  [DECREASE]: state => ({count: state.count - 1}),
-  [INCREASE_BY]: (state, action) => ({count: state.count + action.payload})
-})
+const counter = createReducer(initialState)
+  .handleAction(increase, state => ({ count: state.count + 1 }))
+  .handleAction(decrease, state => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload
+  }));
 
 export default counter;

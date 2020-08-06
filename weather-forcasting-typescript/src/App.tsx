ad9b1ca4af52416b384009c008c6f353673d0fca
Tkendi <acg70159@gmail.com>
2020-08-07 00:18:37 +0900

    city parsing

--- weather-forcasting-typescript/src/App.tsx
+++ weather-forcasting-typescript/src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { Typography } from "@material-ui/core";
 import City from './components/City/cityPicker'
 
 const App = () => {
+  const [nx, setNx] = useState(0)
+  const [ny, setNy] = useState(0)
+  const CityChange = (cityNx: number, cityNy: number) => {
+    setNx(cityNx)
+    setNy(cityNy)
+    console.log(nx, ny)
+  }
   return(
     <div>
       <Typography>Hello Typescript</Typography>
-      <City />
+      <City CityChange = {CityChange} />
     </div>
   )
 }
--- weather-forcasting-typescript/src/components/City/cityPicker.tsx
+++ weather-forcasting-typescript/src/components/City/cityPicker.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { TextField, MenuItem, CssBaseline } from "@material-ui/core";
+import Autocomplete from "@material-ui/lab";
+import data from "../../city.json";
 
-const cityPicker = () => {
+console.log(data.nx)
+console.log(data.ny)
+
+interface Props {
+    CityChange: (nx: number, ny: number) => void;
+}
+
+const cityPicker: React.FC<Props> = (props: Props) => {
   return (
     <React.Fragment>
-      <div>
-        <Typography>Testing</Typography>
+      <div style={{ width: 300 }}>
+        <TextField select label="City" helperText="List of city" onChange={(e) => console.log(data.nx[e.target.value])}>
+          {data.city.map((city, index) => {
+            return (
+              <MenuItem key={city} value={index}>
+                {city}
+              </MenuItem>
+            );
+          })}
+        </TextField>
       </div>
     </React.Fragment>
   );

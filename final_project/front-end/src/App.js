477253d77a8a454dc2bae87a3974c108c49c4171
Tkendi <acg70159@gmail.com>
2020-08-08 02:25:38 +0900

    initialize

---
+++ final_project/front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Typography} from '@material-ui/core'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Typography>Hello</Typography>
+      </div>
+    );
+  }
+}
+
+export default App;

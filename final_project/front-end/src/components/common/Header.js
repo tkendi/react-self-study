59da01781f85de7a53865917aedeaa97d15bab62
Tkendi <acg70159@gmail.com>
2020-08-09 02:02:44 +0900

    style change

--- final_project/front-end/src/components/common/Header.js
+++ final_project/front-end/src/components/common/Header.js
@@ -1,50 +1,8 @@
 import React from "react";
-import Responsive from "./Responsive";
-import { Button, Grid, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import styles from "../../styles/Header.module.css";
-
-// const HeaderBlock = styled.div`
-//   position: adsolute;
-//   width: 100%;
-//   height: 100%;
-//   background: linear-gradient(
-//     to right rgba(200, 200, 220, 0.3),
-//     rgba(250, 250, 224, 0.5),
-//     rgba(255, 255, 250, 0.9)
-//   );
-//   background-size: cover;
-//   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
-// `;
-
-// const Wrapper = styled(Responsive)`
-//   height: 4rem;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   .logo {
-//     font-size: 1.125rem;
-//     font-weight: 800;
-//     letter-spacing: 2px;
-//   }
-//   .right {
-//     display: flex;
-//     align-items: center;
-//   }
-//   .rightText {
-//     padding-right: 15px;
-//     padding-left: 15px;
-//   }
-// `;
-
-// const Spacer = styled.div`
-//   height: 4rem;
-// `;
-
-// const UserInfo = styled.div`
-//   font-weight: 800;
-//   margin-right: 1rem;
-// `;
+import PetsIcon from '@material-ui/icons/Pets';
 
 const Header = ({ user, onLogout }) => {
   return (
@@ -52,16 +10,22 @@ const Header = ({ user, onLogout }) => {
       <div className={styles.HeadBlock}>
         <div className={styles.Wrapper}>
           <Link to="/" className={styles.logo}>
-            TEST
+            <PetsIcon size/>  SoonDoll
           </Link>
+          <div>
+
+          </div>
           {user ? (
             <div className={styles.right}>
-              <div className={styles.userInfo}>{user.useranem}</div>
-              <Button onClick={onLogout}>로그아웃</Button>
+              <div className={styles.userInfo}>
+                {user.useranem}
+                <Typography>님 환영합니다</Typography>
+              </div>
+              <Button onClick={onLogout}>logout</Button>
             </div>
           ) : (
             <div className={styles.right}>
-              <Button href="/login">로그인</Button>
+              <Button href="/login">SIGN IN</Button>
             </div>
           )}
         </div>
@@ -72,28 +36,3 @@ const Header = ({ user, onLogout }) => {
 };
 
 export default Header;
-
-// {user ? (
-//   <div className="right">
-//     <UserInfo>{user.username}</UserInfo>
-//     <Button onClick={onLogout}>로그아웃</Button>
-//   </div>
-// ) : (
-//   <div className="right">
-//     <Button href="/login">로그인</Button>
-//   </div>
-// )}
-
-{
-  /* <Grid container alignItem="center">
-<Button className="rightText" size="large">
-  Streaming
-</Button>
-<Button className="rightText" size="large">
-  Board
-</Button>
-<Button className="rightText" size="large">
-  MyPage
-</Button>
-</Grid> */
-}

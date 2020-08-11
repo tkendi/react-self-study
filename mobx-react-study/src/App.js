69a10121113f9b1bc80f245ef3aef58ec2e24bf1
Tkendi <acg70159@gmail.com>
2020-08-11 18:24:07 +0900

    counter component

--- mobx-react-study/src/App.js
+++ mobx-react-study/src/App.js
@@ -1,4 +1,13 @@
-import React from 'react'
-import {decorate, observable, action, computed} from 'mobx'
-import {observer} from 'mobx-react'
+import React from "react";
+import { decorate, observable, action, computed } from "mobx";
+import { observer } from "mobx-react";
 
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ mobx-react-study/src/Counter.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Counter extends React.Fragment {
+  number = 0;
+
+  increase = () => {
+    this.number++;
+  };
+
+  decrease = () => {
+    this.number--;
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.number}</h1>
+        <button onClick={this.increase}>+1</button>
+        <button onClick={this.decrease}>-1</button>
+      </div>
+    );
+  }
+}
+
+export default Counter; 

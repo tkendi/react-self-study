755e4ab37895f43616cac4f1b5d43e383458215e
Tkendi <acg70159@gmail.com>
2020-08-13 12:20:40 +0900

    redux api setting

--- final_project/front-end/src/components/post/PostViewer.js
+++ final_project/front-end/src/components/post/PostViewer.js
@@ -46,26 +46,36 @@ const PostContent = styled.div`
   color: #424242;
 `;
 
-const PostViewer = () => {
+const PostViewer = ({ post, error, loading }) => {
+  console.log(error);
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewerBlock>존재하지 않는 포스트입니다</PostViewerBlock>;
+    }
+    return <PostViewerBlock>오류발생</PostViewerBlock>;
+  }
+
+  if (loading || !post) {
+    return null;
+  }
+  const { title, body, user, publishedDate, tags } = post;
   return (
     <PostViewerBlock>
       <PostHead>
-        <h1>제목</h1>
+        <h1>{title}</h1>
         <SubInfo>
           <span>
             <b>tester</b>
           </span>
-          <span>{new Date().toLocaleDateString()}</span>
+          <span>{new Date(publishedDate).toLocaleDateString()}</span>
         </SubInfo>
         <Tags>
-          <div className="tag">#태그1</div>
-          <div className="tag">#태그2</div>
-          <div className="tag">#태그3</div>
+          {tags.map((tag) => (
+            <div className="tag">#{tag}</div>
+          ))}
         </Tags>
       </PostHead>
-      <PostContent
-        dangerouslySetInnerHTML={{ __html: "<p>HTML<b>내용</b>입니다.</p>" }}
-      />
+      <PostContent dangerouslySetInnerHTML={{ __html: body }} />
     </PostViewerBlock>
   );
 };

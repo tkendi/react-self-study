76667c0c3c99a42cd7f252c3d19e31b0105cb5e8
Tkendi <acg70159@gmail.com>
2020-08-14 22:09:07 +0900

    code rebuilding

--- final_project/back-end/src/api/posts/posts.ctrl.js
+++ final_project/back-end/src/api/posts/posts.ctrl.js
@@ -1,8 +1,42 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHtml from 'sanitize-html';
+import { filter } from '../../../../../../Users/CHA/AppData/Local/Microsoft/TypeScript/3.9/node_modules/domutils/lib/index';
+import sanitize from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types;
+
+const snaitizeOption = {
+  allowedTags: [
+    'h1',
+    'h2',
+    'b',
+    'i',
+    'u',
+    's',
+    'p',
+    'ul',
+    'ol',
+    'li',
+    'blcokquote',
+    'a',
+    'img',
+  ],
+  allowedAttributes: {
+    a: ['href', 'name', 'target'],
+    img: ['src'],
+    li: ['class'],
+  },
+  allowedSchemes: ['data', 'http'],
+};
+
+const removeHtmlAndShorten = (body) => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
 
 export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
@@ -10,17 +44,17 @@ export const getPostById = async (ctx, next) => {
     ctx.status = 400; //Bad Request 잘못된 요청을 보냈음
     return;
   }
-  try{
+  try {
     const post = await Post.findById(id);
     //포스트 존재하지 않을 시
-    if(!post) {
+    if (!post) {
       ctx.status = 404;
-      return
+      return;
     }
-    ctx.state.post = post
-    return next()
-  }catch(e) {
-    ctx.throw(500, e)
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
   }
 };
 
@@ -41,10 +75,10 @@ export const write = async (ctx) => {
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
-    body,
+    body: sanitizeHtml(body, snaitizeOption),
     tags,
-    user: ctx.state.user
-  }); 
+    user: ctx.state.user,
+  });
   try {
     await post.save();
     ctx.body = post;
@@ -57,35 +91,34 @@ export const list = async (ctx) => {
   const page = parseInt(ctx.query.page || '1', 10);
   if (page < 1) {
     ctx.status = 400;
-    return
+    return;
   }
-  const {tag, username} = ctx.query;
+  const { tag, username } = ctx.query;
   //값이 유효할경우 tag에 넣는다
   const query = {
-    ...(username ? {'user.username' : username} : {}),
-    ...(tag ? {tags: tag} : {})
-  }
-  try{ 
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+  try {
     const posts = await Post.find(query)
-    .sort({_id: -1})
-    .limit(10)
-    .skip((page - 1) * 10)
-    .lean()
-    .exec();
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
     const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts.map(post => ({
+    ctx.body = posts.map((post) => ({
       ...post,
-      body:
-        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+      body: removeHtmlAndShorten(post.body),
     }));
-  } catch(e) {
-    ctx.throw(500, e)
+  } catch (e) {
+    ctx.throw(500, e);
   }
 };
 
 export const read = async (ctx) => {
-  ctx.body = ctx.state.post
+  ctx.body = ctx.state.post;
 };
 
 export const remove = async (ctx) => {
@@ -101,9 +134,13 @@ export const remove = async (ctx) => {
 
 export const update = async (ctx) => {
   const { id } = ctx.params;
+  const nextData = { ...ctx.request.body };
+  if (nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body);
+  }
   try {
     const post = await (
-      await Post.findByIdAndUpdate(id, ctx.request.body, {
+      await Post.findByIdAndUpdate(id, nextData, {
         new: true, //이 값을 설정시 업데이트된 데이터 반환
         //false일 때면 업데이트되기 전의 데이터를 반환
       })
@@ -119,10 +156,10 @@ export const update = async (ctx) => {
 };
 
 export const checkOwnPost = (ctx, next) => {
-  const {user, post} = ctx.state;
-  if(post.user._id.toString() !== user._id) {
+  const { user, post } = ctx.state;
+  if (post.user._id.toString() !== user._id) {
     ctx.status = 403;
-    return
+    return;
   }
-  return next()
-}
+  return next();
+};

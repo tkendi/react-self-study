4a87f9bcf674ecffe7877d7c298c9e9d6b6d163e
Tkendi <acg70159@gmail.com>
2020-08-19 14:35:51 +0900

    style change

--- final_project/front-end/src/App.js
+++ final_project/front-end/src/App.js
@@ -10,32 +10,11 @@ import MainPage from "./components/pages/MainPage";
 import styles from "./styles/App.module.css";
 import styled from "styled-components";
 
-const Container = styled.div`
-  position: fixed;
-
-  padding: 0;
-  margin: 0;
-
-  top: 0;
-  left: 0;
-
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(
-    to right,
-    rgba(70, 70, 100, 0.3) 10%,
-    rgba(250, 250, 250, 0.6) 70%,
-    rgba(80, 90, 75, 0.6)
-  );
-  background-size: cover;
-  font-family: sans-serif;
-`;
-
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Container>
+        <div className = {styles.AppBlock}>
           <Route component={MainPage} path="/" exact={true} />
           <Route
             component={PostListPage}
@@ -47,7 +26,7 @@ class App extends React.Component {
           <Route component={RegisterPage} path="/register" />
           <Route component={WritePage} path="/write" />
           <Route component={PostPage} path="/@:username/:postId" />
-        </Container>
+        </div>
       </React.Fragment>
     );
   }

f064b42d1e5f4872c0652c893a2173c6a8598875
Tkendi <acg70159@gmail.com>
2020-08-20 16:58:06 +0900

    style change

--- final_project/front-end/src/components/main/MainViewer.js
+++ final_project/front-end/src/components/main/MainViewer.js
@@ -13,10 +13,10 @@ const MainViewer = () => {
           <Typography component="h1" variant="h3" className={styles.title}>
             강아지 인 것 같아요
           </Typography>
-          <Typography className={styles.title}>
+          <Typography className = {styles.title}>
             집에 있는 강아지가 보고싶은가요? 혼자 있는 고양이가 걱정되나요?
           </Typography>
-          <Typography className={styles.title}>
+          <Typography className = {styles.title}>
             SoonDoll을 통해 실시간으로 반려동물을 확인하세요
           </Typography>
         </div>

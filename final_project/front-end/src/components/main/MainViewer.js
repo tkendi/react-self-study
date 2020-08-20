51bfb2c5c9f77d871d2e7145606f1d26a8e35451
Tkendi <acg70159@gmail.com>
2020-08-20 21:48:46 +0900

    style change

--- final_project/front-end/src/components/main/MainViewer.js
+++ final_project/front-end/src/components/main/MainViewer.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 import styles from "../../styles/MainViewer.module.css";
+import MainImg from '../../img/MainPage.png'
 
 const MainViewer = () => {
   return (
     <React.Fragment>
+      <img src = {MainImg} alt = "Main" />
       <div className={styles.TitleBlock}>
         <div className={styles.titleForm}>
-          <Typography component="h1" variant="h3" className={styles.title}>
+          <h2 className={styles.textTitle}>
             순돌이는 너무 귀여운
-          </Typography>
-          <Typography component="h1" variant="h3" className={styles.title}>
-            강아지 인 것 같아요
-          </Typography>
-          <Typography className = {styles.title}>
+          </h2>
+          <h2 className={styles.textTitle}>
+            강아지인 것 같아
+          </h2>
+          <Typography className = {styles.textSubTitle}>
             집에 있는 강아지가 보고싶은가요? 혼자 있는 고양이가 걱정되나요?
           </Typography>
-          <Typography className = {styles.title}>
+          <Typography className = {styles.textSubTitle}>
             SoonDoll을 통해 실시간으로 반려동물을 확인하세요
           </Typography>
         </div>

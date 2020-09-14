e027b84708a219c668bb4326a591eeee537532bf
Tkendi <acg70159@gmail.com>
2020-09-15 02:28:30 +0900

    ytsr setting

--- youtube-mp3/Server/src/download/download.ts
+++ youtube-mp3/Server/src/download/download.ts
@@ -4,22 +4,12 @@ import ytsr from 'ytsr';
 
 const router = new Router();
 
-let filter;
-ytsr
-  .getFilters('github')
-  .then(async (filters) => {
-    filter = filters.get('Type').find((o) => o.name === 'Video');
-    let filter2 = await ytsr.getFilters(filter.ref);
-    filter2 = filter2.get('Duration').find((o) => o.name.startsWith('Short'));
-    const options = {
-      limit: 5,
-      nextPageRef: filter2.ref,
-    };
-    const searchResults = await ytsr(null, options);
-    doesth(searchResults);
-  })
-  .catch((err: any) => {
-    console.log(err);
-  });
+router.get('/', async (ctx: any) => {
+  const res = await ytsr(ctx).catch((e: any) => {
+    return Error('Error')
+  
+  const video = res.items.filter(i => i.type === 'video')[0]
+  if(!video) return Error('Error')
+})
 
 export default router;

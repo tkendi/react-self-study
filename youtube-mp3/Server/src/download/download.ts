b001d7557c1536a9726202ba892d33bd30eca89b
Tkendi <acg70159@gmail.com>
2020-09-15 11:07:27 +0900

    ytsr testing

--- youtube-mp3/Server/src/download/download.ts
+++ youtube-mp3/Server/src/download/download.ts
@@ -4,12 +4,20 @@ import ytsr from 'ytsr';
 
 const router = new Router();
 
-router.get('/', async (ctx: any) => {
-  const res = await ytsr(ctx).catch((e: any) => {
-    return Error('Error')
-  
-  const video = res.items.filter(i => i.type === 'video')[0]
-  if(!video) return Error('Error')
-})
+router.get('/:context', async (ctx: any) => {
+  const { context } = ctx.params;
+
+  if (!context) return Error('Empty value');
+
+  const res: any = await ytsr(context).catch((e: any) => {
+    return Error('Error');
+  });
+
+  const video = res.items.filter((i: any) => i.type === 'video')[0]
+  console.log(video)
+  if (!video) return Error('Error');
+
+  console.log(video);
+});
 
 export default router;

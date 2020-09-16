9c5fe1d12259596b6d0f24faf4e7c105835bfaaa
Tkendi <acg70159@gmail.com>
2020-09-16 15:57:50 +0900

    decorator setting

--- youtube-mp3/src/store/Counter.tsx
+++ youtube-mp3/src/store/Counter.tsx
@@ -1,19 +1,22 @@
-import React, { Component } from "react";
-import { decorate, observable, action } from "mobx";
+import React from "react";
+import { observable, action } from "mobx";
 import { observer } from "mobx-react";
 
-class Counter extends Component {
-  number = 0;
+@observer
+class Counter extends React.Component {
+  @observable numbers = 0;
 
+  @action
   increase = () => {
     this.number++;
   };
 
+  @action
   decrease = () => {
     this.number--;
   };
 
-  render() {
+  public render() {
     return (
       <div>
         <h1>{this.number}</h1>
@@ -24,10 +27,4 @@ class Counter extends Component {
   }
 }
 
-decorate(Counter, {
-    number: observable,
-    increase: action,
-    decrease: action
-})
-
-export default observer(Counter)
+export default Counter;

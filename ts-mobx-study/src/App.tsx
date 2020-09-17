92223d851d93c5d9aada3131a1869304eb3f2d75
Tkendi <acg70159@gmail.com>
2020-09-18 01:22:36 +0900

    study mobx

--- ts-mobx-study/src/App.tsx
+++ ts-mobx-study/src/App.tsx
@@ -1,15 +1,53 @@
-import React from "react";
-import Counter from "./components/Counter";
-import SuperMarket from './components/SuperMarket'
+import React, { Component } from 'react';
+import { observer, PropTypes } from 'mobx-react';
+import _ from 'lodash';
 
-function App() {
-  return (
-    <div>
-      <Counter />
-      <hr />
-      <SuperMarket />
-    </div>
-  );
+import Profile from './components/Profile';
+import Selection from './components/Selection';
+
+const propTypes = {
+  store: PropTypes.object
+};
+
+@observer
+class App extends Component {
+
+  componentWillMount() {
+    this.props.store.getUsers();
+  }
+
+  renderProfiles() {
+    return this.props.store.users.map((user: any) => (
+      <Profile 
+        selected={user.id === this.props.store.selectedId}
+        key={user.id}
+        label={user.name}
+        onClick={() => {this.props.store.selectUser(user)}}
+      />
+    ));
+  }
+
+  renderSelection() {
+    if (_.isEmpty(this.props.store.selectedUser)) return null;
+    return (
+      <div className="selection">
+        <Selection user={this.props.store.selectedUser} />
+        <button onClick={this.props.store.clearSelectedUser}>Close Profile</button>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Employee Directory</h3>
+        { this.renderSelection() }
+        { this.renderProfiles() }
+      </div>
+    );
+  }
 }
+
+App.propTypes = propTypes;
 
 export default App;
--- ts-mobx-study/src/useStore.ts
+++
@@ -1,8 +0,0 @@
-import { counter } from "./stores/counter";
-import {market} from './stores/market'
-
-const useStore = () => {
-  return { counter, market };
-};
-
-export default useStore;

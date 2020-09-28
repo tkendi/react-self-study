7de721aea60b8354c7aafe2bca2f3d41399ad704
Tkendi <acg70159@gmail.com>
2020-09-28 21:08:35 +0900

    mobx function modify

--- youtube-mp3/Front/src/store/index.ts
+++ youtube-mp3/Front/src/store/index.ts
@@ -1,12 +1,15 @@
-import {observable, action, computed, configure} from 'mobx'
-import * as searched from '../lib/api/searching'
+import { observable, action, computed, configure } from "mobx";
+import * as searched from "../lib/api/searching";
 
 export default class youtube {
-    @observable searching = ""
+  @observable searching = "";
 
-    @action
-    find = async (search_t: any) => {
-        const data = await searched.search(search_t)
-        console.log(data)
+  @action
+  find = async (search_t: any) => {
+    if (!search_t) {
+    } else {
+      const data = await searched.search(search_t);
+      console.log(data);
     }
+  };
 }

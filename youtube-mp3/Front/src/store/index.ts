f5c6a3aa0d043d3758d7625050caebcc6bc1d7ec
Tkendi <acg70159@gmail.com>
2020-10-01 22:07:24 +0900

    mobx function code change

--- youtube-mp3/Front/src/store/index.ts
+++ youtube-mp3/Front/src/store/index.ts
@@ -2,13 +2,13 @@ import { observable, action, computed, configure, reaction } from "mobx";
 import * as searched from "../lib/api/searching";
 
 export default class youtube {
-  @observable searching = [];
+  @observable searching = "";
 
   @action
   find = async (search_t: any) => {
     if (!search_t) {
     } else {
-      (<any>this.searching.push)= await searched.search(search_t)
+      (<any>this.searching)= await searched.search(search_t)
     }
   };
 }

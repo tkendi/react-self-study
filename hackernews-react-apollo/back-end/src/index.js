f143d63596e93f77a8a18f2d84edd90e757cb369
Tkendi <acg70159@gmail.com>
2020-10-06 14:30:23 +0900

    index change

--- hackernews-react-apollo/back-end/src/index.js
+++ hackernews-react-apollo/back-end/src/index.js
@@ -3,13 +3,33 @@ const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = `
 type Query {
     info: String!
+    fedd: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `;
 
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `Hackersnews Clone API입니다.`,
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
   },
+
+  Link: {
+      id: (parent) => parent.id,
+      description: (parent) => parent.description,
+      url: (parent) => parent.url
+  }
 };
 
 const server = new GraphQLServer({

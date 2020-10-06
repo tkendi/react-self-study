16a2333466f24383aedd15916e090aa01ff2c309
Tkendi <acg70159@gmail.com>
2020-10-06 15:33:59 +0900

    prisma & grapql studying

--- hackernews-react-apollo/back-end/src/index.js
+++ hackernews-react-apollo/back-end/src/index.js
@@ -1,39 +1,35 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-type Query {
-    info: String!
-    fedd: [Link!]!
-}
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`;
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: () => links,
   },
-
-  Link: {
-      id: (parent) => parent.id,
-      description: (parent) => parent.description,
-      url: (parent) => parent.url
-  }
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers,
 });
 server.start(() => console.log(`http://localhost:4000에서 서버 가동중`));

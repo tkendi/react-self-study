0068af2c8752914dad36f83f0547e2d2117c6210
Tkendi <acg70159@gmail.com>
2020-10-08 13:10:46 +0900

    server params change

--- youtube-mp3/back-end/src/api/index.ts
+++ youtube-mp3/back-end/src/api/index.ts
@@ -1,45 +1,8 @@
-import Router from 'koa-router';
-import ytsr from 'ytsr';
+import Router from 'koa-router'
+import find from './search'
 
-const api = new Router();
+const api = new Router()
 
-api.get('/search/:context', async (ctx: any) => {
-  const result = {
-    title: [],
-    link: [],
-  };
+api.use('/search', find.routes())
 
-  const { context } = ctx.params;
-
-  if (!context) return (ctx.body = 'Empty Value');
-
-  const data = await ytsr
-    .getFilters(context)
-    .then(async (filters: any) => {
-      const options = {
-        limit: 5,
-        nextpageRef: filters.get('Type').find((o: any) => o.name === 'Video')
-          .ref,
-      };
-      const res: any = await ytsr(null, options);
-      console.log(res)
-      for (const keys in res.items) {
-        (<any>result.title)[keys] = res.items[keys].title;
-        (<any>result.link)[keys] = res.items[keys].link;
-      }
-    })
-    .catch((err: any) => {
-      console.error(err);
-    });
-
-  // console.log(result);
-
-  ctx.body = result;
-  return result;
-});
-
-api.get('/download/:contenet', async (ctx: any) => {
-  ctx.body = 'Testing Download';
-});
-
-export default api;
+export default api
---
+++ youtube-mp3/back-end/src/api/search.ts
@@ -0,0 +1,41 @@
+import Router from 'koa-router';
+import ytsr from 'ytsr';
+
+const search = new Router();
+
+search.get('/:context', async (ctx: any) => {
+  const result = {
+    title: [],
+    link: [],
+  };
+
+  const { context } = ctx.params;
+
+  if (!context) return (ctx.body = 'Empty Value');
+
+  const data = await ytsr
+    .getFilters(context)
+    .then(async (filters: any) => {
+      const options = {
+        limit: 5,
+        nextpageRef: filters.get('Type').find((o: any) => o.name === 'Video')
+          .ref,
+      };
+      const res: any = await ytsr(null, options);
+      console.log(res)
+      for (const keys in res.items) {
+        (<any>result.title)[keys] = res.items[keys].title;
+        (<any>result.link)[keys] = res.items[keys].link;
+      }
+    })
+    .catch((err: any) => {
+      console.error(err);
+    });
+
+  // console.log(result);
+
+  ctx.body = result;
+  return result;
+});
+
+export default search;

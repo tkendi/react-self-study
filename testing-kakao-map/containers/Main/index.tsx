cb9f132a529f01dcafa6acc35079b91f4d722186
ChaMyungjun <acg70159@gmail.com>
2021-06-03 22:57:01 +0900

    testing geolocation checking

--- testing-kakao-map/containers/Main/index.tsx
+++ testing-kakao-map/containers/Main/index.tsx
@@ -1,26 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 /*global kakao*/
 
 const Main = () => {
-  useEffect(() => {
+  const [position, setPosition] = useState<any>({});
+
+  const createMap = () => {
     var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
     var options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      center: new kakao.maps.LatLng(position.lat, position.lon), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
     new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+  };
+
+  const getGeoLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const lat = pos.coords.latitude;
+          const lon = pos.coords.longitude;
+          setPosition({ lat, lon });
+        },
+        () => console.log("error")
+      );
+    } else {
+      console.log("위치정보 사용 불가능");
+    }
+  };
+
+  useEffect(() => {
+    getGeoLocation();
   }, []);
+
+  useEffect(() => {
+    createMap();
+  }, [position]);
 
   return (
     <>
       <MapWrap>
         <Map id="map" />
         <Category>
-          <CategoryList id="BK9" data-order="0">
+          {/* <CategoryList id="BK9" data-order="0">
             <span className="category_bg bank"></span>
             은행
           </CategoryList>
@@ -43,7 +68,7 @@ const Main = () => {
           <CategoryList id="CS2" data-order="5">
             <span className="category_bg store"></span>
             편의점
-          </CategoryList>
+          </CategoryList> */}
         </Category>
       </MapWrap>
     </>
